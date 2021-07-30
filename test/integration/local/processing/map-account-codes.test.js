3c47296dc7a7146c70504b3e979f3d0d61884f3c
John Watson <johnwatson484@hotmail.com>
2021-07-30 22:04:18 +0100

    Tests added

--- test/integration/local/processing/map-account-codes.test.js
+++ test/integration/local/processing/map-account-codes.test.js
@@ -12,4 +12,40 @@ describe('map account codes', () => {
     await mapAccountCodes(paymentRequest)
     expect(paymentRequest.invoiceLines[0].accountCode).toBe('SOS273')
   })
+
+  test('should map AR code for arable soil gross', async () => {
+    const paymentRequest = {
+      ledger: 'AR',
+      invoiceLines: [{
+        lineDescription: 'G00 - Gross value of claim',
+        schemeCode: '80001'
+      }]
+    }
+    await mapAccountCodes(paymentRequest)
+    expect(paymentRequest.invoiceLines[0].accountCode).toBe('SOS273')
+  })
+
+  test('should map AP code for arable grassland gross', async () => {
+    const paymentRequest = {
+      ledger: 'AP',
+      invoiceLines: [{
+        lineDescription: 'G00 - Gross value of claim',
+        schemeCode: '80002'
+      }]
+    }
+    await mapAccountCodes(paymentRequest)
+    expect(paymentRequest.invoiceLines[0].accountCode).toBe('SOS273')
+  })
+
+  test('should map AR code for arable grassland gross', async () => {
+    const paymentRequest = {
+      ledger: 'AR',
+      invoiceLines: [{
+        lineDescription: 'G00 - Gross value of claim',
+        schemeCode: '80002'
+      }]
+    }
+    await mapAccountCodes(paymentRequest)
+    expect(paymentRequest.invoiceLines[0].accountCode).toBe('SOS273')
+  })
 })
