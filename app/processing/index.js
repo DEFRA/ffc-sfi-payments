8dbf8dd70352e9726c2a7dbee9c84afd41fcc430
John Watson <johnwatson484@hotmail.com>
2021-07-30 12:35:35 +0100

    Check for pending payment requests

--- app/processing/index.js
+++ app/processing/index.js
@@ -1,4 +1,8 @@
+const processPendingPaymentRequests = require('./process-pending-payment-requests')
+const processingInterval = 5000
+
 const start = () => {
+  setInterval(() => processPendingPaymentRequests(), processingInterval)
   console.log('Payment processing started')
 }
 
