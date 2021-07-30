5631ebe1a27d31914dae06fa87016956d8adfe1e
John Watson <johnwatson484@hotmail.com>
2021-07-30 17:52:17 +0100

    Comment updated

--- app/processing/complete-payment-requests.js
+++ app/processing/complete-payment-requests.js
@@ -2,6 +2,7 @@ const db = require('../data')
 
 const completePaymentRequest = async (scheduleId, paymentRequests) => {
   // TODO also create final payment request state in transaction
+  // also check if completed already in case of duplicate processing
   return db.schedule.update({ completed: new Date() }, { where: { scheduleId } })
 }
 
