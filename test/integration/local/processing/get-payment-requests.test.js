a638b0319f1e0859dfca2101192200aea515aa3d
John Watson <johnwatson484@hotmail.com>
2021-07-31 17:29:03 +0100

    Test for duplicates

--- test/integration/local/processing/get-payment-requests.test.js
+++ test/integration/local/processing/get-payment-requests.test.js
@@ -5,6 +5,8 @@ let scheme
 let paymentRequest
 let schedule
 let invoiceLine
+let holdCategory
+let hold
 
 describe('get payment requests', () => {
   beforeEach(async () => {
@@ -39,6 +41,19 @@ describe('get payment requests', () => {
       scheduleId: 1,
       paymentRequestId: 1,
       planned: moment().subtract(1, 'day')
+    }
+
+    holdCategory = {
+      holdCategoryId: 1,
+      schemeId: 1,
+      name: 'Hold'
+    }
+
+    hold = {
+      holdId: 1,
+      holdCategoryId: 1,
+      frn: 1234567890,
+      added: moment().subtract(1, 'day')
     }
   })
 
@@ -242,5 +257,156 @@ describe('get payment requests', () => {
     await db.schedule.create(schedule)
     const paymentRequests = await getPaymentRequests()
     expect(paymentRequests.length).toBe(1)
+  })
+
+  test('should return payment request if scheme has hold category with no holds', async () => {
+    await db.scheme.create(scheme)
+    await db.paymentRequest.create(paymentRequest)
+    await db.invoiceLine.create(invoiceLine)
+    await db.schedule.create(schedule)
+    await db.holdCategory.create(holdCategory)
+    const paymentRequests = await getPaymentRequests()
+    expect(paymentRequests.length).toBe(1)
+  })
+
+  test('should not return payment request if frn on hold', async () => {
+    await db.scheme.create(scheme)
+    await db.paymentRequest.create(paymentRequest)
+    await db.invoiceLine.create(invoiceLine)
+    await db.schedule.create(schedule)
+    await db.holdCategory.create(holdCategory)
+    await db.hold.create(hold)
+    const paymentRequests = await getPaymentRequests()
+    expect(paymentRequests.length).toBe(0)
+  })
+
+  test('should return payment request if hold expired', async () => {
+    await db.scheme.create(scheme)
+    await db.paymentRequest.create(paymentRequest)
+    await db.invoiceLine.create(invoiceLine)
+    await db.schedule.create(schedule)
+    await db.holdCategory.create(holdCategory)
+    hold.closed = new Date()
+    await db.hold.create(hold)
+    const paymentRequests = await getPaymentRequests()
+    expect(paymentRequests.length).toBe(1)
+  })
+
+  test('should return payment request if hold for different customer', async () => {
+    await db.scheme.create(scheme)
+    await db.paymentRequest.create(paymentRequest)
+    await db.invoiceLine.create(invoiceLine)
+    await db.schedule.create(schedule)
+    await db.holdCategory.create(holdCategory)
+    hold.frn = 234567891
+    await db.hold.create(hold)
+    const paymentRequests = await getPaymentRequests()
+    expect(paymentRequests.length).toBe(1)
+  })
+
+  test('should return payment request if hold for different scheme', async () => {
+    await db.scheme.create(scheme)
+    await db.paymentRequest.create(paymentRequest)
+    await db.invoiceLine.create(invoiceLine)
+    await db.schedule.create(schedule)
+    scheme.schemeId = 2
+    await db.scheme.create(scheme)
+    holdCategory.schemeId = 2
+    await db.holdCategory.create(holdCategory)
+    await db.hold.create(hold)
+    const paymentRequests = await getPaymentRequests()
+    expect(paymentRequests.length).toBe(1)
+  })
+
+  test('should remove duplicate payment request if another for same agreement pending', async () => {
+    await db.scheme.create(scheme)
+    await db.paymentRequest.create(paymentRequest)
+    await db.invoiceLine.create(invoiceLine)
+    await db.schedule.create(schedule)
+    paymentRequest.paymentRequestId = 2
+    await db.paymentRequest.create(paymentRequest)
+    invoiceLine.invoiceLineId = 2
+    invoiceLine.paymentRequestId = 2
+    await db.invoiceLine.create(invoiceLine)
+    schedule.scheduleId = 2
+    schedule.paymentRequestId = 2
+    await db.schedule.create(schedule)
+    const paymentRequests = await getPaymentRequests()
+    expect(paymentRequests.length).toBe(1)
+  })
+
+  test('should return first scheduled if payment request if another for same agreement pending', async () => {
+    await db.scheme.create(scheme)
+    await db.paymentRequest.create(paymentRequest)
+    await db.invoiceLine.create(invoiceLine)
+    await db.schedule.create(schedule)
+    paymentRequest.paymentRequestId = 2
+    await db.paymentRequest.create(paymentRequest)
+    invoiceLine.invoiceLineId = 2
+    invoiceLine.paymentRequestId = 2
+    await db.invoiceLine.create(invoiceLine)
+    schedule.scheduleId = 2
+    schedule.paymentRequestId = 2
+    schedule.planned = moment().subtract(2, 'day')
+    await db.schedule.create(schedule)
+    const paymentRequests = await getPaymentRequests()
+    expect(paymentRequests.length).toBe(1)
+    expect(paymentRequests[0].scheduleId).toBe(2)
+  })
+
+  test('should not remove pending for same customer but different marketing year as duplicate', async () => {
+    await db.scheme.create(scheme)
+    await db.paymentRequest.create(paymentRequest)
+    await db.invoiceLine.create(invoiceLine)
+    await db.schedule.create(schedule)
+    paymentRequest.paymentRequestId = 2
+    paymentRequest.marketingYear = 2021
+    await db.paymentRequest.create(paymentRequest)
+    invoiceLine.invoiceLineId = 2
+    invoiceLine.paymentRequestId = 2
+    await db.invoiceLine.create(invoiceLine)
+    schedule.scheduleId = 2
+    schedule.paymentRequestId = 2
+    await db.schedule.create(schedule)
+    const paymentRequests = await getPaymentRequests()
+    expect(paymentRequests.length).toBe(2)
+  })
+
+  test('should not remove pending for different customer as duplicate', async () => {
+    await db.scheme.create(scheme)
+    await db.paymentRequest.create(paymentRequest)
+    await db.invoiceLine.create(invoiceLine)
+    await db.schedule.create(schedule)
+    paymentRequest.paymentRequestId = 2
+    paymentRequest.frn = 1234567891
+    await db.paymentRequest.create(paymentRequest)
+    invoiceLine.invoiceLineId = 2
+    invoiceLine.paymentRequestId = 2
+    await db.invoiceLine.create(invoiceLine)
+    schedule.scheduleId = 2
+    schedule.paymentRequestId = 2
+    await db.schedule.create(schedule)
+    const paymentRequests = await getPaymentRequests()
+    expect(paymentRequests.length).toBe(2)
+  })
+
+  test('should not remove pending for same customer but different scheme as duplicate', async () => {
+    await db.scheme.create(scheme)
+    await db.paymentRequest.create(paymentRequest)
+    await db.invoiceLine.create(invoiceLine)
+    await db.schedule.create(schedule)
+    scheme.schemeId = 2
+    await db.scheme.create(scheme)
+    paymentRequest.paymentRequestId = 2
+    paymentRequest.schemeId = 2
+    await db.paymentRequest.create(paymentRequest)
+    invoiceLine.invoiceLineId = 2
+    invoiceLine.paymentRequestId = 2
+    await db.invoiceLine.create(invoiceLine)
+    schedule.scheduleId = 2
+    schedule.paymentRequestId = 2
+    await db.schedule.create(schedule)
+    const paymentRequests = await getPaymentRequests()
+    expect(paymentRequests.length).toBe(2)
   })
 })
