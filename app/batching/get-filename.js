d2fe97ee3667642acb8f6d97b79bb04b9b169783
John Watson <johnwatson484@hotmail.com>
2021-08-01 17:07:45 +0100

    Filename added

--- app/batching/get-filename.js
+++ app/batching/get-filename.js
@@ -1,5 +1,7 @@
-const getFileName = (batch) => {
+const moment = require('moment')
 
+const getFileName = (batch) => {
+  return `PFELM${batch.sequence.padStart(4, '0')}_${batch.ledger}_${moment(batch.started).format('YYYYMMDDHHmmss')} (SITI).csv`
 }
 
 module.exports = getFileName
