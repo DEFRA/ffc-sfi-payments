ebadb5a7fb2c0cfde46825f5ec3788f3d24555c1
John Watson <johnwatson484@hotmail.com>
2021-08-02 18:27:03 +0100

    Complete batch tests added

--- app/batching/complete-batch.js
+++ app/batching/complete-batch.js
@@ -4,10 +4,9 @@ const completeBatch = async (batchId) => {
   const transaction = await db.sequelize.transaction()
   try {
     const batch = await db.batch.findByPk(batchId, { transaction })
-
     // Check if completed already in case of duplicate processing
     if (batch.published === null) {
-      await db.schedule.update({ published: new Date() }, { where: { batchId }, transaction })
+      await db.batch.update({ published: new Date() }, { where: { batchId }, transaction })
     }
     await transaction.commit()
   } catch (error) {
