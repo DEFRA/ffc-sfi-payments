bf3c886e05a792214ac7d1e1a6ae9744380ba131
John Watson <johnwatson484@hotmail.com>
2021-08-02 22:00:32 +0100

    Removed CSV

--- app/batching/publish-batch.js
+++ app/batching/publish-batch.js
@@ -1,20 +1,3 @@
-const { writeToPath } = require('@fast-csv/format')
-const path = require('path')
-
-const publishBatch = async (batch, filename, content) => {
-  const filepath = path.resolve(__dirname, filename)
-  await writeBatchFile(batch, filepath, content)
-}
-
-const writeBatchFile = async (batch, filepath, content) => {
-  return new Promise(function (resolve, reject) {
-    writeToPath(filepath, content)
-      .on('error', err => reject(err))
-      .on('finish', () => {
-        console.log(`${batch.ledger} batch ${batch.sequence} created`)
-        resolve()
-      })
-  })
-}
+const publishBatch = async (batch, filename, content) => {}
 
 module.exports = publishBatch
