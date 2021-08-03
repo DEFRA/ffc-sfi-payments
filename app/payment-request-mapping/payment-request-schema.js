1017e4694916959812724685f44c32dafd66b0ca
John Watson <johnwatson484@hotmail.com>
2021-08-03 19:31:18 +0100

    Schema updated

--- app/payment-request-mapping/payment-request-schema.js
+++ app/payment-request-mapping/payment-request-schema.js
@@ -4,7 +4,7 @@ module.exports = Joi.object({
   sourceSystem: Joi.string().allow(''),
   deliveryBody: Joi.string().allow(''),
   invoiceNumber: Joi.string().allow(''),
-  frn: Joi.number().optional(),
+  frn: Joi.number().greater(1000000000).less(9999999999).optional(),
   sbi: Joi.number().integer().greater(105000000).less(999999999).optional(),
   paymentRequestNumber: Joi.number().required(),
   agreementNumber: Joi.string().required(),
@@ -13,7 +13,7 @@ module.exports = Joi.object({
   schedule: Joi.string().required(),
   dueDate: Joi.date().required(),
   value: Joi.number().required(),
-  marketingYear: Joi.number().integer().greater(2021).less(2099).optional(),
+  marketingYear: Joi.number().integer().greater(2021).less(2099),
   invoiceLines: Joi.array().required().items(Joi.object({
     standardCode: Joi.number().required(),
     accountCode: Joi.string().required(),
