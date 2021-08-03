eecefe67ce51f584148567a0c8ba6468d14cda26
John Watson <johnwatson484@hotmail.com>
2021-08-03 20:05:55 +0100

    Fix batch date

--- app/batching/get-filename.js
+++ app/batching/get-filename.js
@@ -1,7 +1,7 @@
 const moment = require('moment')
 
 const getFileName = (batch) => {
-  return `${batch.scheme.batchProperties.prefix}${batch.sequence.toString().padStart(4, '0')}_${batch.ledger}_${moment(batch.started).format('YYYYMMDDHHmmss')}${batch.scheme.batchProperties.suffix}.csv`
+  return `${batch.scheme.batchProperties.prefix}${batch.sequence.toString().padStart(4, '0')}_${batch.ledger}_${moment().format('YYYYMMDDHHmmss')}${batch.scheme.batchProperties.suffix}.csv`
 }
 
 module.exports = getFileName
