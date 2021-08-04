f8ca61c382abed3072c10ce9dc1e673fa6b52336
John Watson <johnwatson484@hotmail.com>
2021-08-04 17:27:58 +0100

    Convert to pence

--- app/payment-request-mapping/index.js
+++ app/payment-request-mapping/index.js
@@ -1,6 +1,7 @@
 const db = require('../data')
 const paymentRequestSchema = require('./payment-request-schema')
 const generateInvoiceNumber = require('./generate-invoice-number')
+const { convertToPence } = require('../currency-convert')
 
 async function savePaymentRequest (paymentRequest) {
   const validationResult = paymentRequestSchema.validate(paymentRequest)
@@ -25,6 +26,7 @@ async function savePaymentRequest (paymentRequest) {
       paymentRequest.invoiceNumber = generateInvoiceNumber(paymentRequest)
       paymentRequest.schemeId = await getSchemeId(paymentRequest.sourceSystem, transaction)
       paymentRequest.ledger = paymentRequest.ledger ? paymentRequest.ledger : 'AP'
+      paymentRequest.value = convertToPence(paymentRequest.value)
       const savedPaymentRequest = await db.paymentRequest.create(paymentRequest, { transaction })
       await processInvoiceLines(paymentRequest.invoiceLines, savedPaymentRequest.paymentRequestId, transaction)
       await db.schedule.create({ schemeId: paymentRequest.schemeId, paymentRequestId: savedPaymentRequest.paymentRequestId, planned: new Date() }, { transaction })
@@ -38,6 +40,7 @@ async function savePaymentRequest (paymentRequest) {
 
 async function processInvoiceLines (invoiceLines, paymentRequestId, transaction) {
   for (const invoiceLine of invoiceLines) {
+    invoiceLine.value = convertToPence(invoiceLine.value)
     invoiceLine.schemeCode = await getSchemeCode(invoiceLine.standardCode)
     await db.invoiceLine.create({ paymentRequestId, ...invoiceLine }, { transaction })
   }
