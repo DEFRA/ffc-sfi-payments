7fc2d757ae42b5049149a0356fd86411b548bbc7
John Watson <johnwatson484@hotmail.com>
2021-08-04 22:18:18 +0100

    Whole invoice allocation

---
+++ app/processing/delta/get-unsettled.js
@@ -0,0 +1,16 @@
+const getUnsettled = (previousPaymentRequests) => {
+  const unsettled = []
+  previousPaymentRequests.reduce((x, y) => {
+    if (!x[y.ledger]) {
+      x[y.ledger] = { ledger: y.ledger, value: 0 }
+      unsettled.push(x[y.ledger])
+    }
+    x[y.ledger].value += y.value
+    return x
+  }, {})
+  if (unsettled.some(x => x.value !== 0)) {
+    return unsettled
+  }
+}
+
+module.exports = getUnsettled
--- app/processing/delta/index.js
+++ app/processing/delta/index.js
@@ -3,6 +3,7 @@ const calculateOverallDelta = require('./calculate-overall-delta')
 const determineLedger = require('./determine-ledger')
 const ensureValueConsistency = require('./ensure-value-consistency')
 const getInvoiceLines = require('./get-invoice-lines')
+const getUnsettled = require('./get-unsettled')
 
 const calculateDelta = async (paymentRequest, previousPaymentRequests) => {
   const invoiceLines = getInvoiceLines(paymentRequest, previousPaymentRequests)
@@ -17,6 +18,21 @@ const calculateDelta = async (paymentRequest, previousPaymentRequests) => {
     value: overallDelta,
     ledger: determineLedger(overallDelta),
     invoiceLines: lineDeltas.filter(x => x.value !== 0)
+  }
+
+  const unsettled = getUnsettled(previousPaymentRequests)
+
+  if (unsettled) {
+    if (unsettled.AR.value !== 0 && updatedPaymentRequest.ledger === 'AP') {
+      if ((unsettled.AR.value * -1) > paymentRequest.value) {
+        updatedPaymentRequest.ledger = 'AR'
+      }
+    }
+    if (unsettled.AP.value !== 0 && updatedPaymentRequest.ledger === 'AR') {
+      if (unsettled.AP.value > (paymentRequest.value * -1)) {
+        updatedPaymentRequest.ledger = 'AP'
+      }
+    }
   }
 
   return [updatedPaymentRequest]
