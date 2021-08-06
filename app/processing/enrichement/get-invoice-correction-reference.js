74397b49d6875774a7dfb5305390180daf924116
John Watson <johnwatson484@hotmail.com>
2021-08-06 11:51:18 +0100

    Invoice correction added

--- app/processing/enrichement/get-invoice-correction-reference.js
+++ app/processing/enrichement/get-invoice-correction-reference.js
@@ -1,6 +1,13 @@
 
 const getInvoiceCorrectionReference = (paymentRequests) => {
+  // last AR payment request
+  const lastARId = paymentRequests
+    .filter(x => x.ledger === 'AR')
+    .reduce((x, y) => {
+      return x < y.completedPaymentRequestId ? x : y.completedPaymentRequestId
+    })
 
+  return paymentRequests.find(x => x.completedPaymentRequestId === lastARId)?.invoiceNumber
 }
 
 module.exports = getInvoiceCorrectionReference
