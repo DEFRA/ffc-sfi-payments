bd6edf837d314e38f7c513534db1441c7b73f888
John Watson <johnwatson484@hotmail.com>
2021-08-06 15:08:50 +0100

    Add original invoice dates

--- app/processing/enrichment/get-original-settlement-date.js
+++ app/processing/enrichment/get-original-settlement-date.js
@@ -1,13 +1,16 @@
 
 const getOriginalSettlementDate = (paymentRequests) => {
   // first settlement date of agreement on AP ledger
-  const firstSettlement = paymentRequests
+  const settlementDates = paymentRequests
+    // sequelize returns empty values as null so need null check
     .filter(x => x.ledger === 'AP' && x.settled != null)
-    .reduce((x, y) => {
-      return x.settled > y.settled ? x.settled : y.settled
-    }, 0)
+    .map(x => x.settled)
 
-  return firstSettlement !== 0 ? firstSettlement : undefined
+  if (!settlementDates.length) {
+    return undefined
+  }
+
+  return new Date(Math.min(...settlementDates))
 }
 
 module.exports = getOriginalSettlementDate
