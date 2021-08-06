2d0ae8730f8893c3524bc49c440c4d377ce9a8c4
John Watson <johnwatson484@hotmail.com>
2021-08-06 22:26:28 +0100

    Ensure consistency tests added

---
+++ test/unit/processing/delta/ensure-value-consistency.test.js
@@ -0,0 +1,70 @@
+const ensureValueConsistency = require('../../../../app/processing/delta/ensure-value-consistency')
+
+describe('ensure value consistency', () => {
+  test('should not change lines if value consistent', () => {
+    const paymentRequest = {
+      value: 100,
+      invoiceLines: [{
+        schemeCode: '80001',
+        fundCode: 'DRD10',
+        description: 'G00',
+        value: 99
+      }, {
+        schemeCode: '80001',
+        fundCode: 'DRD10',
+        description: 'G00',
+        value: 1
+      }]
+    }
+
+    ensureValueConsistency(paymentRequest)
+    expect(paymentRequest.invoiceLines[0].value).toBe(99)
+    expect(paymentRequest.invoiceLines[1].value).toBe(1)
+  })
+
+  test('should update first gross lines if value not consistent', () => {
+    const paymentRequest = {
+      value: 100,
+      invoiceLines: [{
+        schemeCode: '80001',
+        fundCode: 'DRD10',
+        description: 'G00',
+        value: 98
+      }, {
+        schemeCode: '80001',
+        fundCode: 'DRD10',
+        description: 'G00',
+        value: 1
+      }]
+    }
+
+    ensureValueConsistency(paymentRequest)
+    expect(paymentRequest.invoiceLines[0].value).toBe(99)
+    expect(paymentRequest.invoiceLines[1].value).toBe(1)
+  })
+
+  test('should update first gross lines if value not consistent when penalties', () => {
+    const paymentRequest = {
+      value: 100,
+      invoiceLines: [{
+        schemeCode: '80001',
+        fundCode: 'DRD10',
+        description: 'G00',
+        value: 97
+      }, {
+        schemeCode: '80001',
+        fundCode: 'DRD10',
+        description: 'P02',
+        value: -1
+      }, {
+        schemeCode: '80001',
+        fundCode: 'DRD10',
+        description: 'G00',
+        value: 1
+      }]
+    }
+
+    ensureValueConsistency(paymentRequest)
+    expect(paymentRequest.invoiceLines[0].value).toBe(100)
+  })
+})
