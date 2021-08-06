5120868df3cf399bd0a4f7f214ab463a244d73af
John Watson <johnwatson484@hotmail.com>
2021-08-06 22:36:58 +0100

    Ledger added

---
+++ test/unit/processing/delta/get-default-ledger.test.js
@@ -0,0 +1,18 @@
+const getDefaultLedger = require('../../../../app/processing/delta/get-default-ledger')
+
+describe('get default ledger', () => {
+  test('should return AP if top up', () => {
+    const defaultLedger = getDefaultLedger(100)
+    expect(defaultLedger).toBe('AP')
+  })
+
+  test('should return AR if recovery', () => {
+    const defaultLedger = getDefaultLedger(-100)
+    expect(defaultLedger).toBe('AR')
+  })
+
+  test('should return AP if zero value', () => {
+    const defaultLedger = getDefaultLedger(0)
+    expect(defaultLedger).toBe('AP')
+  })
+})
