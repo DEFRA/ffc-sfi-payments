a9bf18085c6f9496fe8dd451deb8fc17c4df8849
John Watson <johnwatson484@hotmail.com>
2021-08-06 22:52:07 +0100

    Get lines test added

--- app/processing/delta/get-invoice-lines.js
+++ app/processing/delta/get-invoice-lines.js
@@ -1,9 +1,8 @@
 const getInvoiceLines = (paymentRequest, previousPaymentRequests) => {
-  const invoiceLines = []
   // push all invoice lines from payment request into array
   // for previous requests, do the same but inverse the values so they can later be
   // summed with current to get line deltas
-  invoiceLines.push(paymentRequest.invoiceLines)
+  const invoiceLines = [...paymentRequest.invoiceLines]
   previousPaymentRequests.map(paymentRequest =>
     paymentRequest.invoiceLines.map(invoiceLine => {
       invoiceLine.value *= -1
