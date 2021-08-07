a3f7ea6d9506bf9949e581f9beccf80d59da5075
John Watson <johnwatson484@hotmail.com>
2021-08-07 19:32:12 +0100

    Log updated

--- app/messaging/process-payment-message.js
+++ app/messaging/process-payment-message.js
@@ -2,11 +2,10 @@ const { savePaymentRequest } = require('../inbound')
 
 async function processPaymentMessage (message, receiver) {
   try {
-    console.info('Received request for payment')
     await savePaymentRequest(message.body)
     await receiver.completeMessage(message)
   } catch (err) {
-    console.error('Unable to process message:', err)
+    console.error('Unable to process payment request:', err)
     await receiver.deadLetterMessage(message)
   }
 }
