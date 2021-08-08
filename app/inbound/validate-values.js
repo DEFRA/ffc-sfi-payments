8b7ed50b070541827bb9dc3cf6da04ed336da520
John Watson <johnwatson484@hotmail.com>
2021-08-08 11:40:32 +0100

    Log removed

--- app/inbound/validate-values.js
+++ app/inbound/validate-values.js
@@ -1,6 +1,5 @@
 const validateValues = (value, invoiceLines) => {
   const lineValues = invoiceLines.reduce((x, y) => x + y.value, 0)
-  console.log(value, lineValues)
   if (lineValues !== value) {
     throw new Error('Payment request is invalid. Invoice line values do not match header')
   }
