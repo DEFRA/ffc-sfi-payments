6dda1228ddbbf2f409d92822a49b379fc6b1ee42
John Watson <johnwatson484@hotmail.com>
2021-08-09 18:34:14 +0100

    Fix reference

--- app/inbound/enrichment/enrich-payment-request.js
+++ app/inbound/enrichment/enrich-payment-request.js
@@ -3,7 +3,7 @@ const { createInvoiceNumber } = require('../../invoice-number')
 const getDeliveryBody = require('../get-delivery-body')
 const getFrn = require('../get-frn')
 const getSchemeId = require('../get-scheme-id')
-const { AP } = require('../ledgers')
+const { AP } = require('../../ledgers')
 
 const enrichPaymentRequest = async (paymentRequest, transaction) => {
   paymentRequest.invoiceNumber = createInvoiceNumber(paymentRequest)
