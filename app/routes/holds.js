ee480c595ef9d5527bc46abfbdce05e2e982e0e9
Fay Toward <fay.toward@rpa.gov.uk>
2021-08-12 16:25:15 +0100

    routes added

---
+++ app/routes/holds.js
@@ -0,0 +1,16 @@
+const db = require('../data')
+
+module.exports = [{
+  method: 'GET',
+  path: '/holds',
+  options: {
+    handler: async (request, h) => {
+      await db()
+      const holds = await db.hold.findAll()
+      return h.response({
+        sbi: request.query?.sbi,
+        holds
+      })
+    }
+  }
+}]
