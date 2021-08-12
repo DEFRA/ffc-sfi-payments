07159ac2194e0cadf790ba778e12c4c73eaa1aa0
Fay Toward <fay.toward@rpa.gov.uk>
2021-08-12 16:33:41 +0100

    routes added

--- app/index.js
+++ app/index.js
@@ -18,3 +18,11 @@ module.exports = (async function startService () {
   await paymentProcessing.start()
   await batching.start()
 }())
+
+const createServer = require('./server')
+const init = async () => {
+  const server = await createServer()
+  await server.start()
+  console.log('Server running on %s', server.info.uri)
+}
+init()
