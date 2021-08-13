fd8d3fbcb2ee2515834da087c4a986380f48aed0
fareed-mohammed <fareed.mohammed.uk@gmail.com>
2021-08-13 13:11:41 +0100

    code added to bring back hold categories

--- app/routes/payment-holds.js
+++ app/routes/payment-holds.js
@@ -5,9 +5,31 @@ module.exports = [{
   path: '/payment-holds',
   options: {
     handler: async (request, h) => {
-      const holds = await db.hold.findAll()
+      var holds = await db.hold.findAll({
+        include: [
+          {
+            model: db.holdCategory, as: 'holdCategory'
+          }
+        ]
+      })
+
+      const paymentHolds = []
+
+      if (holds) {
+        for (const hold of holds) {
+          const payment = {
+            holdId: hold.holdId,
+            frn: hold.frn,
+            holdCategoryName: hold.holdCategory.name,
+            dateTimeAdded: hold.added,
+            dateTimeClosed: hold.closed
+          }
+          paymentHolds.push(payment)
+        }
+      }
+
       return h.response({
-        holds
+        paymentHolds
       })
     }
   }
