4115ed64ead95fc7e4d31ee8f7a068463b1b8142
Fay Toward <fay.toward@rpa.gov.uk>
2021-08-26 14:06:41 +0100

    post method

--- app/routes/payment-schemes.js
+++ app/routes/payment-schemes.js
@@ -1,15 +1,16 @@
 const { getPaymentSchemes } = require('../payment-scheme')
-
+ 
 module.exports = [{
-  method: 'GET',
-  path: '/payment-schemes',
-  options: {
-    handler: async (request, h) => {
-      return h.response({
-        getPaymentSchemes
-      })
-    }
-  },
+ method: 'GET',
+ path: '/payment-schemes',
+ options: {
+ handler: async (request, h) => {
+ const paymentSchemes = await getPaymentSchemes()
+ return h.response({
+ paymentSchemes
+ })
+ }
+ },
   {
     method: 'POST',
     path: '/change-status',
