fed1cd7fbcc5859abe01dc2d271c65c909bcc113
John Watson <johnwatson484@hotmail.com>
2022-01-22 19:25:04 +0000

    Add logging

--- app/messaging/process-acknowledgement-message.js
+++ app/messaging/process-acknowledgement-message.js
@@ -1,7 +1,9 @@
 const updateAcknowledgement = require('../acknowledgement')
+const util = require('util')
 
 const processAcknowledgementMessage = async (message, receiver) => {
   try {
+    console.log('Acknowledgement received:', util.inspect(message.body, false, null, true))
     await updateAcknowledgement(message.body)
     await receiver.completeMessage(message)
   } catch (err) {
--- app/messaging/process-return-message.js
+++ app/messaging/process-return-message.js
@@ -1,7 +1,9 @@
 const updateSettlementStatus = require('../settlement')
+const util = require('util')
 
 const processReturnMessage = async (message, receiver) => {
   try {
+    console.log('Return data received:', util.inspect(message.body, false, null, true))
     await updateSettlementStatus(message.body)
     await receiver.completeMessage(message)
   } catch (err) {
