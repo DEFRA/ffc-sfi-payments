c7b211fa4ad7ac0da155faeec64ba100405c2f0d
John Watson <johnwatson484@hotmail.com>
2022-01-24 11:37:20 +0000

    Remove ids if supplied

--- app/inbound/save-invoice-lines.js
+++ app/inbound/save-invoice-lines.js
@@ -2,6 +2,7 @@ const db = require('../data')
 
 const saveInvoiceLines = async (invoiceLines, paymentRequestId, transaction) => {
   for (const invoiceLine of invoiceLines) {
+    delete invoiceLine.invoiceLineId
     await db.invoiceLine.create({ paymentRequestId, ...invoiceLine }, { transaction })
   }
 }
