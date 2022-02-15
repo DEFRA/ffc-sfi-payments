173a84fd7f159397c869f2ee49f55f0db0e9b1df
John Watson <johnwatson484@hotmail.com>
2022-02-15 14:50:02 +0000

    Add additional tests

--- test/unit/processing/delta/get-outstanding-ledger-values.test.js
+++ test/unit/processing/delta/get-outstanding-ledger-values.test.js
@@ -123,4 +123,88 @@ describe('get get outstanding ledger values', () => {
     expect(outstandingLedgerValues.AP).toBe(50)
     expect(outstandingLedgerValues.AR).toBe(0)
   })
+
+  test('should return AP value if AP partially outstanding', () => {
+    const paymentRequests = [{
+      ledger: AP,
+      value: 100,
+      settledValue: 50
+    }]
+    const outstandingLedgerValues = getOutstandingLedgerValues(paymentRequests)
+    expect(outstandingLedgerValues.AP).toBe(50)
+    expect(outstandingLedgerValues.AR).toBe(0)
+  })
+
+  test('should return AR value if AR partially outstanding', () => {
+    const paymentRequests = [{
+      ledger: AP,
+      value: 100,
+      settledValue: 100
+    }, {
+      ledger: AR,
+      value: -100
+    }, {
+      ledger: AR,
+      value: 50
+    }]
+    const outstandingLedgerValues = getOutstandingLedgerValues(paymentRequests)
+    expect(outstandingLedgerValues.AP).toBe(0)
+    expect(outstandingLedgerValues.AR).toBe(50)
+  })
+
+  test('should return values if both partially outstanding', () => {
+    const paymentRequests = [{
+      ledger: AP,
+      value: 100,
+      settledValue: 50
+    }, {
+      ledger: AR,
+      value: -100
+    }, {
+      ledger: AR,
+      value: 50
+    }]
+    const outstandingLedgerValues = getOutstandingLedgerValues(paymentRequests)
+    expect(outstandingLedgerValues.AP).toBe(50)
+    expect(outstandingLedgerValues.AR).toBe(50)
+  })
+
+  test('should sum values if both partially outstanding', () => {
+    const paymentRequests = [{
+      ledger: AP,
+      value: 100,
+      settledValue: 25
+    }, {
+      ledger: AR,
+      value: -100
+    }, {
+      ledger: AP,
+      value: 50,
+      settledValue: 25
+    }, {
+      ledger: AR,
+      value: -25
+    }, {
+      ledger: AR,
+      value: 50
+    }]
+    const outstandingLedgerValues = getOutstandingLedgerValues(paymentRequests)
+    expect(outstandingLedgerValues.AP).toBe(100)
+    expect(outstandingLedgerValues.AR).toBe(75)
+  })
+
+  test('should sum values if past AR reallocation and partially settled', () => {
+    const paymentRequests = [{
+      ledger: AP,
+      value: 100,
+      settledValue: 50
+    }, {
+      ledger: AP,
+      value: -50,
+      settledValue: 0
+    }]
+    const outstandingLedgerValues = getOutstandingLedgerValues(paymentRequests)
+    expect(outstandingLedgerValues.AP).toBe(0)
+    expect(outstandingLedgerValues.AR).toBe(0)
+  })
 })
