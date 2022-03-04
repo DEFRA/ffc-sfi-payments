1aa1ffeed39729d59f1a011dc33aa95be6b9d1fb
John Watson <johnwatson484@hotmail.com>
2022-03-04 21:38:53 +0000

    Add tests

--- test/unit/processing/confirm-due-dates.test.js
+++ test/unit/processing/confirm-due-dates.test.js
@@ -177,4 +177,80 @@ describe('confirm due dates', () => {
     expect(confirmedPaymentRequests[0].dueDate).toBe(moment(new Date()).add(1, 'day').format('DD/MM/YYYY'))
     expect(confirmedPaymentRequests[0].schedule).toBe('M1')
   })
+
+  test('should not update schedule when daily and no outstanding payments', () => {
+    const paymentRequests = [{
+      ledger: AP,
+      dueDate: '09/11/2022',
+      schedule: 'D4',
+      value: -100
+    }]
+    const previousPaymentRequests = [{
+      paymentRequestNumber: 1,
+      ledger: AP,
+      dueDate: moment(new Date()).subtract(2, 'year').format('DD/MM/YYYY'),
+      schedule: 'D4',
+      value: 1000
+    }]
+    const confirmedPaymentRequests = confirmDueDates(paymentRequests, previousPaymentRequests)
+    expect(confirmedPaymentRequests[0].dueDate).toBe(paymentRequests[0].dueDate)
+    expect(confirmedPaymentRequests[0].schedule).toBe(paymentRequests[0].schedule)
+  })
+
+  test('should update schedule to cover remaining payments when daily and three remaining', () => {
+    const paymentRequests = [{
+      ledger: AP,
+      dueDate: '09/11/2022',
+      schedule: 'D4',
+      value: -100
+    }]
+    const previousPaymentRequests = [{
+      paymentRequestNumber: 1,
+      ledger: AP,
+      dueDate: moment(new Date()).format('DD/MM/YYYY'),
+      schedule: 'D4',
+      value: 1000
+    }]
+    const confirmedPaymentRequests = confirmDueDates(paymentRequests, previousPaymentRequests)
+    expect(confirmedPaymentRequests[0].dueDate).toBe(moment(new Date()).add(1, 'day').format('DD/MM/YYYY'))
+    expect(confirmedPaymentRequests[0].schedule).toBe('D3')
+  })
+
+  test('should update schedule to cover remaining payments when daily and two remaining', () => {
+    const paymentRequests = [{
+      ledger: AP,
+      dueDate: '09/11/2022',
+      schedule: 'D4',
+      value: -100
+    }]
+    const previousPaymentRequests = [{
+      paymentRequestNumber: 1,
+      ledger: AP,
+      dueDate: moment(new Date()).subtract(1, 'day').format('DD/MM/YYYY'),
+      schedule: 'D4',
+      value: 1000
+    }]
+    const confirmedPaymentRequests = confirmDueDates(paymentRequests, previousPaymentRequests)
+    expect(confirmedPaymentRequests[0].dueDate).toBe(moment(new Date()).add(1, 'day').format('DD/MM/YYYY'))
+    expect(confirmedPaymentRequests[0].schedule).toBe('D2')
+  })
+
+  test('should update schedule to cover remaining payments when daily and one remaining', () => {
+    const paymentRequests = [{
+      ledger: AP,
+      dueDate: '09/11/2022',
+      schedule: 'D4',
+      value: -100
+    }]
+    const previousPaymentRequests = [{
+      paymentRequestNumber: 1,
+      ledger: AP,
+      dueDate: moment(new Date()).subtract(2, 'day').format('DD/MM/YYYY'),
+      schedule: 'D4',
+      value: 1000
+    }]
+    const confirmedPaymentRequests = confirmDueDates(paymentRequests, previousPaymentRequests)
+    expect(confirmedPaymentRequests[0].dueDate).toBe(moment(new Date()).add(1, 'day').format('DD/MM/YYYY'))
+    expect(confirmedPaymentRequests[0].schedule).toBe('D1')
+  })
 })
