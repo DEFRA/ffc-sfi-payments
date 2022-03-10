4ded6fa2230d749f8209c0d633f32412cd31aed5
John Watson <42574977+johnwatson484@users.noreply.github.com>
2022-03-10 17:10:59 +0000

    Allow disable recoveries (#58)

--- app/processing/process-payment-requests.js
+++ app/processing/process-payment-requests.js
@@ -4,6 +4,7 @@ const completePaymentRequests = require('./complete-payment-requests')
 const transformPaymentRequest = require('./transform-payment-request')
 const routeToRequestEditor = require('./route-to-request-editor')
 const requiresDebtData = require('./requires-debt-data')
+const applyAutoHold = require('./apply-auto-hold')
 
 const processPaymentRequests = async () => {
   const scheduledPaymentRequests = await getPaymentRequests()
@@ -15,15 +16,20 @@ const processPaymentRequests = async () => {
 const processPaymentRequest = async (scheduledPaymentRequest) => {
   const paymentRequests = await transformPaymentRequest(scheduledPaymentRequest.paymentRequest)
 
+  if (await applyAutoHold(paymentRequests)) {
+    return
+  }
+
   // If has AR but no debt enrichment data, then route to request editor and apply hold
   if (requiresDebtData(paymentRequests)) {
     await routeToRequestEditor(scheduledPaymentRequest.paymentRequest)
-  } else {
-    for (const paymentRequest of paymentRequests) {
-      await mapAccountCodes(paymentRequest)
-    }
-    await completePaymentRequests(scheduledPaymentRequest.scheduleId, paymentRequests)
+    return
   }
+
+  for (const paymentRequest of paymentRequests) {
+    await mapAccountCodes(paymentRequest)
+  }
+  await completePaymentRequests(scheduledPaymentRequest.scheduleId, paymentRequests)
 }
 
 module.exports = processPaymentRequests
