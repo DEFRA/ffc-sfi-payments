6eb2a2aca3ab9f379d27ecb37bda1930575f3bc0
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-03-21 11:06:36 +0000

    Retrieve schedule and transform with dates etc..

--- app/messaging/update-requests-awaiting-manual-ledger-check.js
+++ app/messaging/update-requests-awaiting-manual-ledger-check.js
@@ -1,5 +1,8 @@
 const db = require('../data')
 const { getHoldCategoryId } = require('../holds')
+const enrichPaymentRequests = require('../processing/enrichment')
+const confirmDueDates = require('../processing/confirm-due-dates')
+const getCompletedPaymentRequests = require('../processing/get-completed-payment-requests')
 const completePaymentRequests = require('../processing/complete-payment-requests')
 const mapAccountCodes = require('../processing/map-account-codes')
 
@@ -11,15 +14,37 @@ const updateRequestsAwaitingManualLedgerCheck = async (paymentRequest) => {
     throw new Error(`No payment request matching invoice number: ${paymentRequest.invoiceNumber}`)
   }
 
-  const { scheduleId, paymentRequests } = paymentRequest
+  const paymentRequestId = checkPaymentRequest.paymentRequestId
+  const schedule = await getScheduleId(paymentRequestId)
 
-  // Mapping account codes need to be re-calculated on processing of a manual ledger check
-  for (const paymentRequestItem of paymentRequests) {
-    await mapAccountCodes(paymentRequestItem)
+  if (schedule) {
+    const scheduleId = schedule.scheduleId
+    const paymentRequests = await transformPaymentRequest(orginalPaymentRequest, paymentRequest.paymentRequests)
+
+    // Mapping account codes need to be re-calculated on processing of a manual ledger check
+    for (const paymentRequestItem of paymentRequests) {
+      await mapAccountCodes(paymentRequestItem)
+    }
+
+    const updatedPaymentRequests = paymentRequests.map(x => {
+      delete x.paymentRequestId
+      return x
+    })
+
+    await completePaymentRequests(scheduleId, updatedPaymentRequests)
+    await removeHold(paymentRequest.schemeId, paymentRequest.frn)
   }
+}
 
-  await completePaymentRequests(scheduleId, paymentRequests)
-  await removeHold(paymentRequest.schemeId, paymentRequest.frn)
+const transformPaymentRequest = async (paymentRequest, paymentRequests) => {
+  const previousPaymentRequests = await getCompletedPaymentRequests(paymentRequest.schemeId, paymentRequest.frn, paymentRequest.marketingYear, paymentRequest.agreementNumber, paymentRequest.paymentRequestNumber)
+  const confirmedPaymentRequests = confirmDueDates(paymentRequests, previousPaymentRequests)
+  paymentRequests = enrichPaymentRequests(confirmedPaymentRequests, previousPaymentRequests)
+  return paymentRequests
+}
+
+const getScheduleId = (paymentRequestId) => {
+  return db.schedule.findOne({ where: { paymentRequestId, completed: null } })
 }
 
 async function removeHold (schemeId, frn) {
