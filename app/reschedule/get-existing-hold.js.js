edd82c0448b31b6d90b304adbd9c407462bc61ac
John Watson <johnwatson484@hotmail.com>
2022-03-23 12:59:53 +0000

    Add functionality

--- app/reschedule/get-existing-hold.js.js
+++ app/reschedule/get-existing-hold.js.js
@@ -4,7 +4,6 @@ const getExistingHold = async (holdCategoryId, frn, transaction) => {
   return db.hold.findOne({
     transaction,
     lock: true,
-    skipLocked: true,
     where: { holdCategoryId, frn, closed: null }
   })
 }
