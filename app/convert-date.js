17222adb63ecf9c29a6a3ff9d1fb3fac79659147
John Watson <johnwatson484@hotmail.com>
2022-03-24 16:54:22 +0000

    Fix date format of original settlement date

--- app/convert-date.js
+++ app/convert-date.js
@@ -1,6 +1,6 @@
 function convertDateToDDMMYYYY (dd, mm, yyyy) {
   const day = String(dd).padStart(2, '0')
-  const month = String(mm).padStart(2, '0')
+  const month = String(mm + 1).padStart(2, '0')
   const year = String(yyyy)
 
   return `${day}/${month}/${year}`
