0fdef926e03b8e6ac8ae81ee4f5013ad39d68530
John Watson <johnwatson484@hotmail.com>
2022-03-28 16:25:19 +0100

    Update tests

--- app/settlement.js
+++ app/settlement.js
@@ -2,7 +2,24 @@ const db = require('./data')
 
 const updateSettlementStatus = async (returnData) => {
   if (returnData.settled) {
-    await db.completedPaymentRequest.update({ lastSettlement: returnData.settlementDate, settledValue: db.Sequelize.literal(`COALESCE("settledValue", 0) + ${returnData.value}`) }, { where: { invoiceNumber: returnData.invoiceNumber } })
+    await db.completedPaymentRequest.update({
+      lastSettlement: returnData.settlementDate,
+      settledValue: returnData.value
+    }, {
+      where: {
+        invoiceNumber: returnData.invoiceNumber,
+        [db.Sequelize.Op.or]:
+          [{
+            lastSettlement: {
+              [db.Sequelize.Op.is]: null
+            }
+          }, {
+            lastSettlement: {
+              [db.Sequelize.Op.lt]: returnData.settlementDate
+            }
+          }]
+      }
+    })
   }
 }
 
