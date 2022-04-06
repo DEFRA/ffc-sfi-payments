0f8967ac5ad54812a805763ed2efaeac631d84fc
John Watson <johnwatson484@hotmail.com>
2022-04-06 08:28:24 +0100

    Retain logging

--- app/server.js
+++ app/server.js
@@ -20,9 +20,9 @@ async function createServer () {
   // Register the plugins
   await server.register(require('./plugins/errors'))
   await server.register(require('./plugins/router'))
+  await server.register(require('./plugins/logging'))
   if (config.isDev) {
     await server.register(require('blipp'))
-    await server.register(require('./plugins/logging'))
   }
 
   return server
