4de20b25029ce804150cb59f8e97bef4c4fcce58
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-05-27 10:49:21 +0100

    Add error capture on settlement request

--- app/event/send-processing-return-event.js
+++ app/event/send-processing-return-event.js
@@ -6,16 +6,34 @@ const sendProcessingReturnEvent = async (message) => {
   const invoiceNumber = message.invoiceNumber
   const frn = message.frn
 
-  const { correlationId, paymentRequestNumber, agreementNumber } = await getPaymentSchemeByInvoiceAndFrn(invoiceNumber, frn)
+  const completedPaymentRequest = await getPaymentSchemeByInvoiceAndFrn(invoiceNumber, frn)
 
-  const event = {
-    id: correlationId ?? uuidv4(),
+  if (completedPaymentRequest) {
+    await raiseCompletedReturn(completedPaymentRequest)
+  } else {
+    await raiseError(invoiceNumber, frn)
+  }
+}
+
+const returnEvent = () => {
+  return {
     name: 'payment-request-return',
     type: 'return',
-    message: 'Settlement received from DAX',
-    data: { returnMessage: message, paymentRequestNumber, agreementNumber, frn }
+    message: 'Settlement received from DAX'
   }
+}
+
+const raiseCompletedReturn = async (completedPaymentRequest) => {
+  const { correlationId, paymentRequestNumber, agreementNumber } = completedPaymentRequest
+  const event = { ...returnEvent(), id: correlationId, data: { paymentRequestNumber, agreementNumber } }
   await raiseEvent(event)
+}
+
+const raiseError = async (invoiceNumber, frn) => {
+  const errorMessage = `Unable to find settlement for payment request ${invoiceNumber} and frn ${frn}`
+  const event = { ...returnEvent(), id: uuidv4, data: { invoiceNumber, frn, errorMessage }, type: 'error' }
+  await raiseEvent(event)
+  console.error(errorMessage)
 }
 
 module.exports = sendProcessingReturnEvent
