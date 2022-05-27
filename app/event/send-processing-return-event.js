8f8a4bd909e2df9088126fa0f6811e23ba24e251
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-05-27 11:59:07 +0100

    Update check on settlement

--- app/event/send-processing-return-event.js
+++ app/event/send-processing-return-event.js
@@ -2,14 +2,12 @@ const raiseEvent = require('./raise-event')
 const { v4: uuidv4 } = require('uuid')
 const getPaymentSchemeByInvoiceAndFrn = require('../processing/get-payment-request-by-invoice-frn')
 
-const sendProcessingReturnEvent = async (message) => {
+const sendProcessingReturnEvent = async (message, isError = false) => {
   const invoiceNumber = message.invoiceNumber
   const frn = message.frn
 
-  const completedPaymentRequest = await getPaymentSchemeByInvoiceAndFrn(invoiceNumber, frn)
-
-  if (completedPaymentRequest) {
-    await raiseCompletedReturnEvent(completedPaymentRequest)
+  if (!isError) {
+    await raiseCompletedReturnEvent(invoiceNumber, frn)
   } else {
     await raiseErrorEvent(invoiceNumber, frn)
   }
@@ -23,18 +21,22 @@ const returnEvent = () => {
   }
 }
 
-const raiseCompletedReturnEvent = async (completedPaymentRequest) => {
-  const { correlationId, paymentRequestNumber, agreementNumber } = completedPaymentRequest
-  const id = correlationId ?? uuidv4()
-  const event = { ...returnEvent(), id, data: { paymentRequestNumber, agreementNumber } }
-  await raiseEvent(event)
+const raiseCompletedReturnEvent = async (invoiceNumber, frn) => {
+  const completedPaymentRequest = await getPaymentSchemeByInvoiceAndFrn(invoiceNumber, frn)
+  if (completedPaymentRequest) {
+    const { correlationId, paymentRequestNumber, agreementNumber } = completedPaymentRequest
+    const id = correlationId ?? uuidv4()
+    const event = { ...returnEvent(), id, data: { paymentRequestNumber, agreementNumber } }
+    await raiseEvent(event)
+  } else {
+    await raiseErrorEvent(invoiceNumber, frn)
+  }
 }
 
 const raiseErrorEvent = async (invoiceNumber, frn) => {
   const errorMessage = `Unable to find settlement for payment request ${invoiceNumber} and frn ${frn}`
   const event = { ...returnEvent(), id: uuidv4(), data: { invoiceNumber, frn, errorMessage }, type: 'error' }
   await raiseEvent(event)
-  throw new Error(errorMessage)
 }
 
 module.exports = sendProcessingReturnEvent
--- app/messaging/process-return-message.js
+++ app/messaging/process-return-message.js
@@ -4,13 +4,15 @@ const util = require('util')
 const processReturnMessage = async (message, receiver) => {
   try {
     console.log('Return data received:', util.inspect(message.body, false, null, true))
-    await updateSettlementStatus(message.body)
-    await receiver.completeMessage(message)
-    console.log('Settlement statuses updated from return file')
-  } catch (err) {
-    if (err.message.includes('Unable to find settlement for payment request')) {
+    const settlementCompleted = await updateSettlementStatus(message.body)
+    if (settlementCompleted) {
+      await receiver.completeMessage(message)
+      console.log('Settlement statuses updated from return file')
+    } else {
       await receiver.deadLetterMessage(message)
+      console.error('Unable to find settlement for payment request', message.body)
     }
+  } catch (err) {
     console.error('Unable to process return request:', err)
   }
 }
