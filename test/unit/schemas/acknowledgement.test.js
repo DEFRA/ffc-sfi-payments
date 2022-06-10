131841057c75041ae7926f993053a8e2cb4ecd49
Marc Templeton <marcte@kainos.com>
2022-06-10 10:57:05 +0100

    removing allowUnknown: false from ack tests due to future flexiblity

--- test/unit/schemas/acknowledgement.test.js
+++ test/unit/schemas/acknowledgement.test.js
@@ -13,57 +13,39 @@ describe('acknowledgement schema', () => {
   })
 
   test('should not return error key when an acknowledgement is given', async () => {
-    const { error } = acknowledgementSchema.required().validate(acknowledgement, {
-      abortEarly: false,
-      allowUnknown: false
-    })
+    const { error } = acknowledgementSchema.required().validate(acknowledgement, { abortEarly: false })
     expect(error).toBeUndefined()
   })
 
   test('should not return error key when an acknowledgement with only required keys is given', async () => {
     delete acknowledgement.message
 
-    const { error } = acknowledgementSchema.required().validate(acknowledgement, {
-      abortEarly: false,
-      allowUnknown: false
-    })
+    const { error } = acknowledgementSchema.required().validate(acknowledgement, { abortEarly: false })
 
     expect(error).toBeUndefined()
   })
 
   test('should return error key when an empty acknowledgement is given', async () => {
-    const { error } = acknowledgementSchema.required().validate({}, {
-      abortEarly: false,
-      allowUnknown: false
-    })
+    const { error } = acknowledgementSchema.required().validate({}, { abortEarly: false })
     expect(error).toBeDefined()
   })
 
   test('should return 4 errors when an empty acknowledgement is given', async () => {
-    const { error } = acknowledgementSchema.required().validate({}, {
-      abortEarly: false,
-      allowUnknown: false
-    })
+    const { error } = acknowledgementSchema.required().validate({}, { abortEarly: false })
     expect(error.details.length).toBe(4)
   })
 
   test('should return error key when an acknowledgement with only optional keys is given', async () => {
     acknowledgement = { ...acknowledgement.message }
 
-    const { error } = acknowledgementSchema.required().validate({}, {
-      abortEarly: false,
-      allowUnknown: false
-    })
+    const { error } = acknowledgementSchema.required().validate({}, { abortEarly: false })
     expect(error).toBeDefined()
   })
 
   test('should return 4 errors when an acknowledgement with only optional keys is given', async () => {
     acknowledgement = { ...acknowledgement.message }
 
-    const { error } = acknowledgementSchema.required().validate({}, {
-      abortEarly: false,
-      allowUnknown: false
-    })
+    const { error } = acknowledgementSchema.required().validate({}, { abortEarly: false })
     expect(error.details.length).toBe(4)
   })
 })
