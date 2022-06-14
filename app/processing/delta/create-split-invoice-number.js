fca207e0573e9f1c79a32c9114bb0ff484b42501
John Watson <johnwatson484@hotmail.com>
2022-06-14 11:54:38 +0100

    Use more variables

--- app/processing/delta/create-split-invoice-number.js
+++ app/processing/delta/create-split-invoice-number.js
@@ -10,11 +10,18 @@ const createSplitInvoiceNumber = (invoiceNumber, splitId, schemeId) => {
 }
 
 const createSitiAgriInvoiceNumber = (invoiceNumber, splitId) => {
-  return `${invoiceNumber.slice(0, 8)}${splitId}${invoiceNumber.slice(8, invoiceNumber.length - 3)}${invoiceNumber.slice(invoiceNumber.length - 2)}`
+  // an invoice number is a string made up of three elements, to inject a split id character into the string we need to identify the position of those elements
+  // we also need to trim the first character of the final element
+  const firstElementLength = 8
+  const finalElementLength = 3
+  return `${invoiceNumber.slice(0, firstElementLength)}${splitId}${invoiceNumber.slice(firstElementLength, invoiceNumber.length - finalElementLength)}${invoiceNumber.slice(invoiceNumber.length - finalElementLength + 1)}`
 }
 
 const createDefaultInvoiceNumber = (invoiceNumber, splitId) => {
-  return `${invoiceNumber.slice(0, invoiceNumber.length - 4)}${splitId}${invoiceNumber.slice(invoiceNumber.length - 4, invoiceNumber.length - 3)}${invoiceNumber.slice(invoiceNumber.length - 2)}`
+  // an invoice number is a string made up of two elements, to inject a split id character into the string we need to identify the position of those elements
+  // we also need to trim the first character of the final element
+  const finalElementLength = 3
+  return `${invoiceNumber.slice(0, invoiceNumber.length - finalElementLength - 1)}${splitId}${invoiceNumber.slice(invoiceNumber.length - finalElementLength - 1, invoiceNumber.length - finalElementLength)}${invoiceNumber.slice(invoiceNumber.length - finalElementLength + 1)}`
 }
 
 module.exports = createSplitInvoiceNumber
