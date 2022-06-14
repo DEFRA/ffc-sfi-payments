387f489ced89af7038f69dcd4cc48813888ea37b
John Watson <johnwatson484@hotmail.com>
2022-06-14 11:59:55 +0100

    Update variables

--- app/processing/delta/create-split-invoice-number.js
+++ app/processing/delta/create-split-invoice-number.js
@@ -13,15 +13,18 @@ const createSitiAgriInvoiceNumber = (invoiceNumber, splitId) => {
   // an invoice number is a string made up of three elements, to inject a split id character into the string we need to identify the position of those elements
   // we also need to trim the first character of the final element
   const firstElementLength = 8
-  const finalElementLength = 3
-  return `${invoiceNumber.slice(0, firstElementLength)}${splitId}${invoiceNumber.slice(firstElementLength, invoiceNumber.length - finalElementLength)}${invoiceNumber.slice(invoiceNumber.length - finalElementLength + 1)}`
+  const originalFinalElementLength = 3
+  const newFinalElementLength = 2
+  return `${invoiceNumber.slice(0, firstElementLength)}${splitId}${invoiceNumber.slice(firstElementLength, invoiceNumber.length - originalFinalElementLength)}${invoiceNumber.slice(invoiceNumber.length - newFinalElementLength)}`
 }
 
 const createDefaultInvoiceNumber = (invoiceNumber, splitId) => {
   // an invoice number is a string made up of two elements, to inject a split id character into the string we need to identify the position of those elements
   // we also need to trim the first character of the final element
-  const finalElementLength = 3
-  return `${invoiceNumber.slice(0, invoiceNumber.length - finalElementLength - 1)}${splitId}${invoiceNumber.slice(invoiceNumber.length - finalElementLength - 1, invoiceNumber.length - finalElementLength)}${invoiceNumber.slice(invoiceNumber.length - finalElementLength + 1)}`
+  const finalElementIndexLength = 4
+  const originalFinalElementLength = 3
+  const newFinalElementLength = 2
+  return `${invoiceNumber.slice(0, invoiceNumber.length - finalElementIndexLength)}${splitId}${invoiceNumber.slice(invoiceNumber.length - finalElementIndexLength, invoiceNumber.length - originalFinalElementLength)}${invoiceNumber.slice(invoiceNumber.length - newFinalElementLength)}`
 }
 
 module.exports = createSplitInvoiceNumber
