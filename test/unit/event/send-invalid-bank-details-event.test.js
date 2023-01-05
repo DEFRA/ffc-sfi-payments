a64fd005ffee37a442d2a17a9453f32e52b12931
kirsten.williamson <kirsten.williamson@kainos.com>
2023-01-05 15:55:10 +0000

    Added unit tests for file send-invalid-bank-details-event.js

---
+++ test/unit/event/send-invalid-bank-details-event.test.js
@@ -0,0 +1,212 @@
+const mockUUID = require('../../mock-uuid')
+const mockFRN = require('../../mock-frn')
+
+jest.mock('uuid')
+const { v4: uuidv4 } = require('uuid')
+uuidv4.mockReturnValue(mockUUID)
+
+jest.mock('../../../app/event/raise-event')
+const raiseEvent = require('../../../app/event/raise-event')
+
+const sendInvalidBankDetailsEvent = require('../../../app/event/send-invalid-bank-details-event')
+
+let mockEvent
+
+describe('send invalid bank details event', () => {
+  beforeEach(async () => {
+    uuidv4.mockReturnValue(mockUUID)
+
+    mockEvent = {
+      id: mockUUID,
+      name: 'invalid-bank-details',
+      type: 'error',
+      message: 'No valid bank details held',
+      data: { mockFRN }
+    }
+  })
+
+  afterEach(async () => {
+    jest.resetAllMocks()
+    jest.resetModules()
+  })
+
+  test('should call uuidv4 when a frn is given', async () => {
+    await sendInvalidBankDetailsEvent(mockFRN)
+    expect(uuidv4).toHaveBeenCalled()
+  })
+
+  test('should call uuidv4 when an empty frn is given', async () => {
+    await sendInvalidBankDetailsEvent({})
+    expect(uuidv4).toHaveBeenCalled()
+  })
+
+  test('should call uuidv4 when undefined is given', async () => {
+    await sendInvalidBankDetailsEvent(undefined)
+    expect(uuidv4).toHaveBeenCalled()
+  })
+
+  test('should call uuidv4 when null is given', async () => {
+    await sendInvalidBankDetailsEvent(null)
+    expect(uuidv4).toHaveBeenCalled()
+  })
+
+  test('should call uuidv4 when an empty string is given', async () => {
+    await sendInvalidBankDetailsEvent('')
+    expect(uuidv4).toHaveBeenCalled()
+  })
+
+  test('should call uuidv4 once when a frn is given', async () => {
+    await sendInvalidBankDetailsEvent(mockFRN)
+    expect(uuidv4).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call uuidv4 once when an empty frn is given', async () => {
+    await sendInvalidBankDetailsEvent({})
+    expect(uuidv4).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call uuidv4 once when undefined is given', async () => {
+    await sendInvalidBankDetailsEvent(undefined)
+    expect(uuidv4).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call uuidv4 once when null is given', async () => {
+    await sendInvalidBankDetailsEvent(null)
+    expect(uuidv4).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call uuidv4 once when an empty string is given', async () => {
+    await sendInvalidBankDetailsEvent('')
+    expect(uuidv4).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call uuidv4 with no parameters when a frn is given', async () => {
+    await sendInvalidBankDetailsEvent(mockFRN)
+    expect(uuidv4).toHaveBeenCalledWith()
+  })
+
+  test('should call uuidv4 with no parameters when an empty frn is given', async () => {
+    await sendInvalidBankDetailsEvent({})
+    expect(uuidv4).toHaveBeenCalledWith()
+  })
+
+  test('should call uuidv4 with no parameters when undefined is given', async () => {
+    await sendInvalidBankDetailsEvent(undefined)
+    expect(uuidv4).toHaveBeenCalledWith()
+  })
+
+  test('should call uuidv4 with no parameters when null is given', async () => {
+    await sendInvalidBankDetailsEvent(null)
+    expect(uuidv4).toHaveBeenCalledWith()
+  })
+
+  test('should call uuidv4 with no parameters when an empty string is given', async () => {
+    await sendInvalidBankDetailsEvent('')
+    expect(uuidv4).toHaveBeenCalledWith()
+  })
+
+  test('should call raiseEvent when a frn is given', async () => {
+    await sendInvalidBankDetailsEvent(mockFRN)
+    expect(raiseEvent).toHaveBeenCalled()
+  })
+
+  test('should call raiseEvent when an empty frn is given', async () => {
+    await sendInvalidBankDetailsEvent({})
+    expect(raiseEvent).toHaveBeenCalled()
+  })
+
+  test('should call raiseEvent when undefined is given', async () => {
+    await sendInvalidBankDetailsEvent(undefined)
+    expect(raiseEvent).toHaveBeenCalled()
+  })
+
+  test('should call raiseEvent when null is given', async () => {
+    await sendInvalidBankDetailsEvent(null)
+    expect(raiseEvent).toHaveBeenCalled()
+  })
+
+  test('should call raiseEvent when an empty string is given', async () => {
+    await sendInvalidBankDetailsEvent('')
+    expect(raiseEvent).toHaveBeenCalled()
+  })
+
+  test('should call raiseEvent once when a frn is given', async () => {
+    await sendInvalidBankDetailsEvent(mockFRN)
+    expect(raiseEvent).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call raiseEvent once when an empty frn is given', async () => {
+    await sendInvalidBankDetailsEvent({})
+    expect(raiseEvent).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call raiseEvent once when undefined is given', async () => {
+    await sendInvalidBankDetailsEvent(undefined)
+    expect(raiseEvent).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call raiseEvent once when null is given', async () => {
+    await sendInvalidBankDetailsEvent(null)
+    expect(raiseEvent).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call raiseEvent once when an empty string is given', async () => {
+    await sendInvalidBankDetailsEvent('')
+    expect(raiseEvent).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call raiseEvent with mockEvent when a frn is given', async () => {
+    mockEvent.data = { frn: mockFRN }
+    await sendInvalidBankDetailsEvent(mockFRN)
+    expect(raiseEvent).toHaveBeenCalledWith(mockEvent)
+  })
+
+  test('should call raiseEvent with mockEvent with empty frn data when an empty frn is given', async () => {
+    mockEvent.data = { frn: {} }
+    await sendInvalidBankDetailsEvent({})
+    expect(raiseEvent).toHaveBeenCalledWith(mockEvent)
+  })
+
+  test('should call raiseEvent with mockEvent with undefined frn data when undefined is given', async () => {
+    mockEvent.data = { frn: mockFRN }
+    await sendInvalidBankDetailsEvent(mockFRN)
+    expect(raiseEvent).toHaveBeenCalledWith(mockEvent)
+  })
+
+  test('should call raiseEvent with mockEvent with null frn data when null is given', async () => {
+    mockEvent.data = { frn: null }
+    await sendInvalidBankDetailsEvent(null)
+    expect(raiseEvent).toHaveBeenCalledWith(mockEvent)
+  })
+
+  test('should call raiseEvent with mockEvent with empty string frn data when an empty string is given', async () => {
+    mockEvent.data = { frn: '' }
+    await sendInvalidBankDetailsEvent('')
+    expect(raiseEvent).toHaveBeenCalledWith(mockEvent)
+  })
+
+  test('should return undefined when a frn is given', async () => {
+    const result = await sendInvalidBankDetailsEvent(mockFRN)
+    expect(result).toBeUndefined()
+  })
+
+  test('should return undefined when an empty frn is given', async () => {
+    const result = await sendInvalidBankDetailsEvent({})
+    expect(result).toBeUndefined()
+  })
+
+  test('should return undefined when undefined is given', async () => {
+    const result = await sendInvalidBankDetailsEvent(undefined)
+    expect(result).toBeUndefined()
+  })
+
+  test('should return undefined when null is given', async () => {
+    const result = await sendInvalidBankDetailsEvent(null)
+    expect(result).toBeUndefined()
+  })
+
+  test('should return undefined when an empty string is given', async () => {
+    const result = await sendInvalidBankDetailsEvent('')
+    expect(result).toBeUndefined()
+  })
+})
