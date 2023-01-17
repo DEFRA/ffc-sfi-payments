c8d9bb96c2711adf3727fb7b2985a39d508a7852
kirsten.williamson <kirsten.williamson@kainos.com>
2023-01-17 17:01:53 +0000

    Committing work done on remaining suggested changes

--- test/unit/acknowledgement/process-invalid.test.js
+++ test/unit/acknowledgement/process-invalid.test.js
@@ -1,3 +1,21 @@
+const mockCommit = jest.fn()
+const mockRollback = jest.fn()
+const mockTransaction = {
+  commit: mockCommit,
+  rollback: mockRollback
+}
+
+jest.mock('../../../app/data', () => {
+  return {
+    sequelize:
+       {
+         transaction: jest.fn().mockImplementation(() => {
+           return { ...mockTransaction }
+         })
+       }
+  }
+})
+
 jest.mock('../../../app/config')
 const mockConfig = require('../../../app/config')
 
@@ -30,13 +48,13 @@ let mockResetPaymentRequestById
 let schemeId
 let paymentRequestId
 
-describe('send acknowledgement event', () => {
+describe('send acknowledgement error event', () => {
   beforeEach(() => {
     mockConfig.isAlerting = true
 
     mockAcknowledgement = JSON.parse(JSON.stringify(require('../../mock-acknowledgement')))
     mockAcknowledgementError = JSON.parse(JSON.stringify(require('../../mock-acknowledgement-error')))
-    mockHoldCategoryName = 'DAX rejection'
+    mockHoldCategoryName = JSON.parse(JSON.stringify(require('../../constants/hold-categories-names')))
 
     schemeId = SFI
     paymentRequestId = 1
@@ -59,8 +77,72 @@ describe('send acknowledgement event', () => {
   })
 
   afterEach(() => {
-    jest.resetAllMocks()
-    jest.resetModules()
+    jest.clearAllMocks()
+  })
+
+  test('should return undefined when processInvalid is called', async () => {
+    const result = await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError)
+    expect(result).toBeUndefined()
+  })
+
+  test('should call resetPaymentRequestById when a schemeId, paymentRequestId, frn and transaction are given', async () => {
+    await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError)
+    expect(resetPaymentRequestById).toHaveBeenCalled()
+  })
+
+  test('should call resetPaymentRequestById once when a schemeId, paymentRequestId, frn and transaction are given', async () => {
+    await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError)
+    expect(resetPaymentRequestById).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call resetPaymentRequestById with schemeId, paymentRequestId, frn and transaction when a schemeId, paymentRequestId, frn and transaction are given', async () => {
+    await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError)
+    expect(resetPaymentRequestById).toHaveBeenCalledWith(paymentRequestId, schemeId, mockTransaction)
+  })
+
+  test('should call getHoldCategoryName when an unsuccessful ack object message is given', async () => {
+    await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError)
+    expect(getHoldCategoryName).toHaveBeenCalled()
+  })
+
+  test('should call getHoldCategoryName once when an unsuccessful ack object message is given', async () => {
+    await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError)
+    expect(getHoldCategoryName).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call getHoldCategoryName with an unsuccessful ack object message when an unsuccessful ack object message is given', async () => {
+    await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError)
+    expect(getHoldCategoryName).toHaveBeenCalledWith(mockAcknowledgementError.message)
+  })
+
+  test('should call getHoldCategoryId when a schemeId, holdCategoryName and transaction is given', async () => {
+    await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError)
+    expect(getHoldCategoryId).toHaveBeenCalled()
+  })
+
+  test('should call getHoldCategoryId once when a schemeId, holdCategoryName and transaction is given', async () => {
+    await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError)
+    expect(getHoldCategoryId).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call getHoldCategoryId with a schemeId, holdCategoryName and transaction when a schemeId, holdCategoryName and transaction is given', async () => {
+    await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError)
+    expect(getHoldCategoryId).toHaveBeenCalledWith(schemeId, mockHoldCategoryName, mockTransaction)
+  })
+
+  test('should call holdAndReschedule when a schemeId, paymentRequestId, holdCategoryId, frn and transaction is given', async () => {
+    await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError)
+    expect(holdAndReschedule).toHaveBeenCalled()
+  })
+
+  test('should call holdAndReschedule once when a schemeId, paymentRequestId, holdCategoryId, frn and transaction is given', async () => {
+    await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError)
+    expect(holdAndReschedule).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call holdAndReschedule with a schemeId, paymentRequestId, holdCategoryId, frn and transaction when a schemeId, paymentRequestId, holdCategoryId, frn and transaction is given', async () => {
+    await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError)
+    expect(holdAndReschedule).toHaveBeenCalledWith(schemeId, paymentRequestId, mockHoldCategoryId, mockFRN, mockTransaction)
   })
 
   test('should call sendAckowledgementErrorEvent when an unsuccessful ack object is given and isAlerting is true', async () => {
@@ -93,5 +175,68 @@ describe('send acknowledgement event', () => {
     mockConfig.isAlerting = false
     await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgement)
     expect(mockEvent.sendAckowledgementErrorEvent).not.toHaveBeenCalled()
+  })
+
+  test('should call mockTransaction.commit when an unsuccessful ack object is given', async () => {
+    await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError)
+    expect(mockTransaction.commit).toHaveBeenCalled()
+  })
+
+  test('should call mockTransaction.commit once when an unsuccessful ack object is given', async () => {
+    await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError)
+    expect(mockTransaction.commit).toHaveBeenCalledTimes(1)
+  })
+
+  test('should not call mockTransaction.rollback when an unsuccessful ack object is given and nothing throws', async () => {
+    await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError)
+    expect(mockTransaction.rollback).not.toHaveBeenCalled()
+  })
+
+  test('should call mockTransaction.rollback when resetPaymentRequestById throws', async () => {
+    resetPaymentRequestById.mockRejectedValue(new Error())
+    try { await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError) } catch { }
+    expect(mockTransaction.rollback).toHaveBeenCalled()
+  })
+
+  test('should call mockTransaction.rollback once when resetPaymentRequestById throws', async () => {
+    resetPaymentRequestById.mockRejectedValue(new Error())
+    try { await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError) } catch { }
+    expect(mockTransaction.rollback).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call mockTransaction.rollback when getHoldCategoryId throws', async () => {
+    getHoldCategoryId.mockRejectedValue(new Error())
+    try { await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError) } catch { }
+    expect(mockTransaction.rollback).toHaveBeenCalled()
+  })
+
+  test('should call mockTransaction.rollback once when getHoldCategoryId throws', async () => {
+    getHoldCategoryId.mockRejectedValue(new Error())
+    try { await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError) } catch { }
+    expect(mockTransaction.rollback).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call mockTransaction.rollback when holdAndReschedule throws', async () => {
+    holdAndReschedule.mockRejectedValue(new Error())
+    try { await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError) } catch { }
+    expect(mockTransaction.rollback).toHaveBeenCalled()
+  })
+
+  test('should call mockTransaction.rollback once when holdAndReschedule throws', async () => {
+    holdAndReschedule.mockRejectedValue(new Error())
+    try { await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError) } catch { }
+    expect(mockTransaction.rollback).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call mockTransaction.rollback when mockTransaction.commit throws', async () => {
+    mockTransaction.commit.mockRejectedValue(new Error())
+    try { await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError) } catch { }
+    expect(mockTransaction.rollback).toHaveBeenCalled()
+  })
+
+  test('should call mockTransaction.rollback once when mockTransaction.commit throws', async () => {
+    mockTransaction.commit.mockRejectedValue(new Error())
+    try { await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError) } catch { }
+    expect(mockTransaction.rollback).toHaveBeenCalledTimes(1)
   })
 })
--- test/unit/event/send-acknowledgement-error-event.test.js
+++ test/unit/event/send-acknowledgement-error-event.test.js
@@ -6,6 +6,7 @@ const sendProcessingAckInvalidBankDetailsErrorEvent = require('../../../app/even
 
 const mockFRN = require('../../mock-frn')
 const sendAckowledgementErrorEvent = require('../../../app/event/send-acknowledgement-error-event')
+const { BANK_ACCOUNT_ANOMALY, DAX_REJECTION } = require('../../constants/hold-categories-names')
 
 let mockAcknowledgementError
 let mockHoldCategoryName
@@ -13,7 +14,7 @@ let mockHoldCategoryName
 describe('send acknowledgement event', () => {
   beforeEach(() => {
     mockAcknowledgementError = JSON.parse(JSON.stringify(require('../../mock-acknowledgement-error')))
-    mockHoldCategoryName = 'DAX rejection'
+    mockHoldCategoryName = JSON.parse(JSON.stringify(require('../../constants/hold-categories-names')))
   })
 
   afterEach(() => {
@@ -22,35 +23,59 @@ describe('send acknowledgement event', () => {
   })
 
   test('should call sendProcessingAckErrorEvent when a holdCategoryName, an unsuccessful ack object and frn is given and holdCategoryName is "DAX rejection"', async () => {
+    mockHoldCategoryName = DAX_REJECTION
     await sendAckowledgementErrorEvent(mockHoldCategoryName, mockAcknowledgementError, mockFRN)
     expect(sendProcessingAckErrorEvent).toHaveBeenCalled()
   })
 
   test('should call sendProcessingAckErrorEvent once when a holdCategoryName, an unsuccessful ack object and frn is given and holdCategoryName is "DAX rejection"', async () => {
+    mockHoldCategoryName = DAX_REJECTION
     await sendAckowledgementErrorEvent(mockHoldCategoryName, mockAcknowledgementError, mockFRN)
     expect(sendProcessingAckErrorEvent).toHaveBeenCalledTimes(1)
   })
 
   test('should call sendProcessingAckErrorEvent with the unsuccessful ack object when a holdCategoryName, an unsuccessful ack object and frn is given and holdCategoryName is "DAX rejection"', async () => {
+    mockHoldCategoryName = DAX_REJECTION
     await sendAckowledgementErrorEvent(mockHoldCategoryName, mockAcknowledgementError, mockFRN)
     expect(sendProcessingAckErrorEvent).toHaveBeenCalledWith(mockAcknowledgementError)
   })
 
   test('should call sendProcessingAckInvalidBankDetailsErrorEvent when a holdCategoryName, an unsuccessful ack object and frn is given and holdCategoryName is "Bank account anomaly"', async () => {
-    mockHoldCategoryName = 'Bank account anomaly'
+    mockHoldCategoryName = BANK_ACCOUNT_ANOMALY
     await sendAckowledgementErrorEvent(mockHoldCategoryName, mockAcknowledgementError, mockFRN)
     expect(sendProcessingAckInvalidBankDetailsErrorEvent).toHaveBeenCalled()
   })
 
   test('should call sendProcessingAckInvalidBankDetailsErrorEvent once when a holdCategoryName, an unsuccessful ack object and frn is given and holdCategoryName is "Bank account anomaly"', async () => {
-    mockHoldCategoryName = 'Bank account anomaly'
+    mockHoldCategoryName = BANK_ACCOUNT_ANOMALY
     await sendAckowledgementErrorEvent(mockHoldCategoryName, mockAcknowledgementError, mockFRN)
     expect(sendProcessingAckInvalidBankDetailsErrorEvent).toHaveBeenCalledTimes(1)
   })
 
   test('should call sendProcessingAckInvalidBankDetailsErrorEvent with the unsuccessful ack object when a holdCategoryName, an unsuccessful ack object and frn is given and holdCategoryName is "Bank account anomaly"', async () => {
-    mockHoldCategoryName = 'Bank account anomaly'
+    mockHoldCategoryName = BANK_ACCOUNT_ANOMALY
     await sendAckowledgementErrorEvent(mockHoldCategoryName, mockAcknowledgementError, mockFRN)
     expect(sendProcessingAckInvalidBankDetailsErrorEvent).toHaveBeenCalledWith(mockFRN)
   })
+  /*
+  test('should throw error with "Sequelize transaction issue" when mockTransaction.commit throws error with "Sequelize transaction commit issue"', async () => {
+    sendProcessingAckInvalidBankDetailsErrorEvent.mockRejectedValue(new Error())
+    const wrapper = async () => {
+      await sendAckowledgementErrorEvent(mockHoldCategoryName, mockAcknowledgementError, mockFRN)
+    }
+    expect(wrapper).rejects.toThrow()
+  })
+
+  test('should call mockTransaction.rollback when mockTransaction.commit throws', async () => {
+    sendProcessingAckInvalidBankDetailsErrorEvent.mockRejectedValue(new Error())
+    try { await sendAckowledgementErrorEvent(mockHoldCategoryName, mockAcknowledgementError, mockFRN) } catch { }
+    expect(sendProcessingAckInvalidBankDetailsErrorEvent).toHaveBeenCalled()
+  })
+
+  test('should call mockTransaction.rollback once when mockTransaction.commit throws', async () => {
+    mockTransaction.commit.mockRejectedValue(new Error())
+    try { await processInvalid(schemeId, paymentRequestId, mockFRN, mockAcknowledgementError) } catch { }
+    expect(mockTransaction.rollback).toHaveBeenCalledTimes(1)
+  })
+  */
 })
