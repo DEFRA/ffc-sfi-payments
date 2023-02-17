c9bd0018cb825a64b1b86d7bf06940bc40a8f492
John Watson <johnwatson484@hotmail.com>
2023-02-17 22:13:54 +0000

    Add events

--- app/event/send-processing-error-event.js
+++ app/event/send-processing-error-event.js
@@ -1,7 +1,18 @@
 const raiseEvent = require('./raise-event')
 const { v4: uuidv4 } = require('uuid')
+const config = require('../config')
+const { EventPublisher } = require('ffc-pay-event-publisher')
 
 const sendProcessingErrorEvent = async (paymentRequest, error) => {
+  if (config.useV1Events) {
+    await sendV1ProcessingErrorEvent(paymentRequest, error)
+  }
+  if (config.useV2Events) {
+    await sendV2ProcessingErrorEvent(paymentRequest, error)
+  }
+}
+
+const sendV1ProcessingErrorEvent = async (paymentRequest, error) => {
   const correlationId = paymentRequest?.correlationId ?? uuidv4()
   const event = {
     id: correlationId,
@@ -11,6 +22,19 @@ const sendProcessingErrorEvent = async (paymentRequest, error) => {
     data: { paymentRequest }
   }
   await raiseEvent(event, 'error', error)
+}
+
+const sendV2ProcessingErrorEvent = async (paymentRequest, error) => {
+  const event = {
+    source: 'ffc-pay-processing',
+    type: 'uk.gov.defra.ffc.pay.warning.processing.failed',
+    data: {
+      message: error.message,
+      paymentRequest
+    }
+  }
+  const eventPublisher = new EventPublisher(config.eventsTopic)
+  await eventPublisher.publishEvent(event)
 }
 
 module.exports = sendProcessingErrorEvent
