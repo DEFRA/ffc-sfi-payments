35440d0e809364dfa4c1f0b6a299ab3a66e9d211
John Watson <johnwatson484@hotmail.com>
2023-02-17 22:16:35 +0000

    Add events

--- app/event/send-processing-ack-invalid-bank-details-error-event.js
+++ app/event/send-processing-ack-invalid-bank-details-error-event.js
@@ -1,7 +1,18 @@
 const raiseEvent = require('./raise-event')
 const { v4: uuidv4 } = require('uuid')
+const config = require('../config')
+const { EventPublisher } = require('ffc-pay-event-publisher')
 
 const sendProcessingAckInvalidBankDetailsErrorEvent = async (frn) => {
+  if (config.useV1Events) {
+    await sendV1ProcessingAckInvalidBankDetailsErrorEvent(frn)
+  }
+  if (config.useV2Events) {
+    await sendV2ProcessingAckInvalidBankDetailsErrorEvent(frn)
+  }
+}
+
+const sendV1ProcessingAckInvalidBankDetailsErrorEvent = async (frn) => {
   const event = {
     id: uuidv4(),
     name: 'invalid-bank-details',
@@ -10,6 +21,18 @@ const sendProcessingAckInvalidBankDetailsErrorEvent = async (frn) => {
     data: { frn }
   }
   await raiseEvent(event)
+}
+
+const sendV2ProcessingAckInvalidBankDetailsErrorEvent = async (frn) => {
+  const event = {
+    source: 'ffc-pay-processing',
+    type: 'uk.gov.defra.ffc.pay.warning.bank.missing',
+    data: {
+      frn
+    }
+  }
+  const eventPublisher = new EventPublisher(config.eventsTopic)
+  await eventPublisher.publishEvent(event)
 }
 
 module.exports = sendProcessingAckInvalidBankDetailsErrorEvent
