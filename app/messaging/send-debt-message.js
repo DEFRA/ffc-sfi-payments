d03608652dd2798d30dd93cb50e15c809fcc4145
John Watson <john.watson2@rpa.gov.uk>
2023-02-20 16:12:30 +0000

    Update events

--- app/messaging/send-debt-message.js
+++ app/messaging/send-debt-message.js
@@ -4,7 +4,7 @@ const util = require('util')
 
 const sendDebtMessage = async (paymentRequest) => {
   const sender = new MessageSender(config.debtTopic)
-  await sender.sendMessage({ body: paymentRequest, type: 'uk.gov.pay.debt', source: 'ffc-pay-processing' })
+  await sender.sendMessage({ body: paymentRequest, type: 'uk.gov.defra.ffc.pay.debt', source: 'ffc-pay-processing' })
   await sender.closeConnection()
   console.log('Payment request sent for debt data:', util.inspect(paymentRequest, false, null, true))
 }
