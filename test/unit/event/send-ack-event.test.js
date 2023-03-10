5ff9978cefa3cb01575171b38a4c9a187dc6eab2
John Watson <john.watson2@rpa.gov.uk>
2023-03-10 17:16:58 +0000

    Add ack tests

---
+++ test/unit/event/send-ack-event.test.js
@@ -0,0 +1,128 @@
+const mockSendEvent = jest.fn()
+const mockPublishEvent = jest.fn()
+const MockPublishEvent = jest.fn().mockImplementation(() => {
+  return {
+    sendEvent: mockSendEvent
+  }
+})
+const MockEventPublisher = jest.fn().mockImplementation(() => {
+  return {
+    publishEvent: mockPublishEvent
+  }
+})
+jest.mock('ffc-pay-event-publisher', () => {
+  return {
+    PublishEvent: MockPublishEvent,
+    EventPublisher: MockEventPublisher
+  }
+})
+jest.mock('../../../app/processing/get-payment-request-by-invoice-frn')
+const getPaymentSchemeByInvoiceAndFrn = require('../../../app/processing/get-payment-request-by-invoice-frn')
+jest.mock('../../../app/config')
+const config = require('../../../app/config')
+const { PAYMENT_ACKNOWLEDGED } = require('../../../app/constants/events')
+const { SOURCE } = require('../../../app/constants/source')
+const sendAckEvent = require('../../../app/event/send-ack-event')
+
+let paymentRequest
+let acknowledgement
+
+beforeEach(() => {
+  paymentRequest = JSON.parse(JSON.stringify(require('../../mocks/payment-request')))
+  acknowledgement = JSON.parse(JSON.stringify(require('../../mocks/acknowledgement')))
+
+  getPaymentSchemeByInvoiceAndFrn.mockResolvedValue(paymentRequest)
+
+  config.useV1Events = true
+  config.useV2Events = true
+  config.eventTopic = 'v1-events'
+  config.eventsTopic = 'v2-events'
+})
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
+describe('V1 ack event', () => {
+  test('should send V1 event if V1 events enabled', async () => {
+    config.useV1Events = true
+    await sendAckEvent(acknowledgement)
+    expect(mockSendEvent).toHaveBeenCalled()
+  })
+
+  test('should not send V1 event if V1 events disabled', async () => {
+    config.useV1Events = false
+    await sendAckEvent(acknowledgement)
+    expect(mockSendEvent).not.toHaveBeenCalled()
+  })
+
+  test('should send event to V1 topic', async () => {
+    await sendAckEvent(acknowledgement)
+    expect(MockPublishEvent.mock.calls[0][0]).toBe(config.eventTopic)
+  })
+
+  test('should generate a new uuid as Id if payment request does not have correlation Id', async () => {
+    paymentRequest.correlationId = undefined
+    await sendAckEvent(acknowledgement)
+    expect(mockSendEvent.mock.calls[0][0].properties.id).toMatch(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i)
+  })
+
+  test('should raise payment-request-acknowledged event name', async () => {
+    await sendAckEvent(acknowledgement)
+    expect(mockSendEvent.mock.calls[0][0].name).toBe('payment-request-acknowledged')
+  })
+
+  test('should raise success status event', async () => {
+    await sendAckEvent(acknowledgement)
+    expect(mockSendEvent.mock.calls[0][0].properties.status).toBe('success')
+  })
+
+  test('should raise error event type', async () => {
+    await sendAckEvent(acknowledgement)
+    expect(mockSendEvent.mock.calls[0][0].properties.action.type).toBe('info')
+  })
+
+  test('should include ack message in event', async () => {
+    await sendAckEvent(acknowledgement)
+    expect(mockSendEvent.mock.calls[0][0].properties.action.message).toBe('Payment request acknowledged by DAX')
+  })
+
+  test('should include acknowledgement in event', async () => {
+    await sendAckEvent(acknowledgement)
+    expect(mockSendEvent.mock.calls[0][0].properties.action.data).toEqual(acknowledgement)
+  })
+})
+
+describe('V2 ack error event', () => {
+  test('should send V2 event if V2 events enabled', async () => {
+    config.useV2Events = true
+    await sendAckEvent(acknowledgement)
+    expect(mockPublishEvent).toHaveBeenCalled()
+  })
+
+  test('should not send V2 event if V2 events disabled', async () => {
+    config.useV2Events = false
+    await sendAckEvent(acknowledgement)
+    expect(mockPublishEvent).not.toHaveBeenCalled()
+  })
+
+  test('should send event to V2 topic', async () => {
+    await sendAckEvent(acknowledgement)
+    expect(MockEventPublisher.mock.calls[0][0]).toBe(config.eventsTopic)
+  })
+
+  test('should raise an event with processing source', async () => {
+    await sendAckEvent(acknowledgement)
+    expect(mockPublishEvent.mock.calls[0][0].source).toBe(SOURCE)
+  })
+
+  test('should raise acknowledged payment event type', async () => {
+    await sendAckEvent(acknowledgement)
+    expect(mockPublishEvent.mock.calls[0][0].type).toBe(PAYMENT_ACKNOWLEDGED)
+  })
+
+  test('should include payment request in event data', async () => {
+    await sendAckEvent(acknowledgement)
+    expect(mockPublishEvent.mock.calls[0][0].data).toEqual(paymentRequest)
+  })
+})
