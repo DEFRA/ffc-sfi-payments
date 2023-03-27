2ba3021c5385c06bab44926ecb1312874847f602
John Watson <42574977+johnwatson484@users.noreply.github.com>
2023-03-27 15:55:17 +0100

    Fix format of hold event (#107)

--- app/holds/remove-hold.js
+++ app/holds/remove-hold.js
@@ -4,7 +4,7 @@ const { sendHoldEvent } = require('../event')
 
 const removeHold = async (holdId) => {
   await db.hold.update({ closed: Date.now() }, { where: { holdId } })
-  const hold = await db.hold.findByPk(holdId)
+  const hold = await db.hold.findOne({ where: holdId, raw: true })
   await sendHoldEvent(hold, REMOVED)
 }
 
