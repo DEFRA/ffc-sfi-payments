08b5a4976181200511da8921cdce9437aac3d61a
John Watson <john.watson2@rpa.gov.uk>
2023-03-27 11:11:44 +0100

    Fix format of hold event

--- app/holds/add-hold.js
+++ app/holds/add-hold.js
@@ -4,7 +4,7 @@ const { sendHoldEvent } = require('../event')
 
 const addHold = async (frn, holdCategoryId, transaction) => {
   const hold = await db.hold.create({ frn, holdCategoryId, added: Date.now() }, { transaction })
-  await sendHoldEvent(hold, ADDED)
+  await sendHoldEvent(hold.get({ plain: true }), ADDED)
 }
 
 module.exports = addHold
