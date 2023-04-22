6c44b5d0ab57fc619e6ff0384367c0a46e22dac3
John Watson <johnwatson484@hotmail.com>
2023-04-22 15:50:28 +0100

    Add tests

--- test/integration/local/processing/confirm-payment-request-number.test.js
+++ test/integration/local/processing/confirm-payment-request-number.test.js
@@ -39,15 +39,27 @@ describe('confirm payment request number', () => {
   test('should retain existing payment request number if previous payment request lower number', async () => {
     await db.scheme.create(scheme)
     await db.paymentRequest.create(paymentRequest)
+    await db.completedPaymentRequest.create(paymentRequest)
     paymentRequest.paymentRequestNumber = 2
     const paymentRequestNumber = await confirmPaymentRequestNumber(paymentRequest)
     expect(paymentRequestNumber).toBe(paymentRequest.paymentRequestNumber)
   })
 
-  test('should increment payment request number by 1 if previous payment request equal number', async () => {
+  test('should increment payment request number if previous payment request equal number', async () => {
     await db.scheme.create(scheme)
     await db.paymentRequest.create(paymentRequest)
+    await db.completedPaymentRequest.create(paymentRequest)
     const paymentRequestNumber = await confirmPaymentRequestNumber(paymentRequest)
     expect(paymentRequestNumber).toBe(paymentRequest.paymentRequestNumber + 1)
+  })
+
+  test('should increment payment request number if previous payment request higher number', async () => {
+    await db.scheme.create(scheme)
+    paymentRequest.paymentRequestNumber = 2
+    await db.paymentRequest.create(paymentRequest)
+    await db.completedPaymentRequest.create(paymentRequest)
+    paymentRequest.paymentRequestNumber = 1
+    const paymentRequestNumber = await confirmPaymentRequestNumber(paymentRequest)
+    expect(paymentRequestNumber).toBe(3)
   })
 })
