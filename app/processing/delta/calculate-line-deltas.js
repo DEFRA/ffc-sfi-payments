30d99158396875480b00a780471899dc46ab7de8
Kirsten Williamson <kirsten.williamson@kainos.com>
2023-05-10 14:24:59 +0100

    Add new constants

--- app/processing/delta/calculate-line-deltas.js
+++ app/processing/delta/calculate-line-deltas.js
@@ -1,11 +1,13 @@
+const { DEFAULT } = require('../../../app/constants/default')
+
 const calculateLineDeltas = (invoiceLines) => {
   return [...invoiceLines.reduce((x, y) => {
     // group by line types, so create key representing the combination
     // exclude account code as past requests vary based on ledger
-    const key = `${y.schemeCode}-${y.fundCode}-${y.agreementNumber ?? 'default'}-${y.description}`
+    const key = `${y.schemeCode}-${y.fundCode}-${y.agreementNumber ?? DEFAULT}-${y.description}`
 
     // if key doesn't exist then first instance so create new group
-    const item = x.get(key) || Object.assign({}, { schemeCode: y.schemeCode, fundCode: y.fundCode, agreementNumber: y.agreementNumber ?? 'default', description: y.description, value: 0 })
+    const item = x.get(key) || Object.assign({}, { schemeCode: y.schemeCode, fundCode: y.fundCode, agreementNumber: y.agreementNumber ?? DEFAULT, description: y.description, value: 0 })
     item.value += Number(y.value)
 
     return x.set(key, item)
