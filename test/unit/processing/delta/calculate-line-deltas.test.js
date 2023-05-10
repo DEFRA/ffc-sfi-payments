3a1866c52380477f9b269c4bbcc9d8fa2506e4f0
Kirsten1822 <113036956+Kirsten1822@users.noreply.github.com>
2023-05-10 14:35:52 +0100

    177143/Handle-MAOASSBI-claims (#120)
    * Update version in package.json
    
    * Support to handle MAOASSBI claims
    
    * PPA group by agreement number
    
    * Restructure some test files
    
    * Up jest limit
    
    * Add new constants

--- test/unit/processing/delta/calculate-line-deltas.test.js
+++ test/unit/processing/delta/calculate-line-deltas.test.js
@@ -5,11 +5,13 @@ describe('calculate line deltas', () => {
     const invoiceLines = [{
       schemeCode: '80001',
       fundCode: 'DRD10',
+      agreementNumber: 'SIP123456789012',
       description: 'G00',
       value: 10
     }, {
       schemeCode: '80001',
       fundCode: 'DRD10',
+      agreementNumber: 'SIP123456789012',
       description: 'G00',
       value: -8
     }]
@@ -22,21 +24,76 @@ describe('calculate line deltas', () => {
     const invoiceLines = [{
       schemeCode: '80001',
       fundCode: 'DRD10',
+      agreementNumber: 'SIP123456789012',
       description: 'G00',
       value: 10
     }, {
       schemeCode: '80001',
       fundCode: 'DRD10',
+      agreementNumber: 'SIP123456789012',
       description: 'G00',
       value: -8
     }, {
       schemeCode: '80002',
       fundCode: 'DRD10',
+      agreementNumber: 'SIP123456789012',
       description: 'G00',
       value: 11
     }, {
       schemeCode: '80002',
       fundCode: 'DRD10',
+      agreementNumber: 'SIP123456789012',
+      description: 'G00',
+      value: -7
+    }]
+
+    const lineDeltas = calculateLineDeltas(invoiceLines)
+    expect(lineDeltas.find(x => x.schemeCode === '80001').value).toBe(2)
+    expect(lineDeltas.find(x => x.schemeCode === '80002').value).toBe(4)
+  })
+
+  test('should calculate delta values by group when agreementNumber is undefined', () => {
+    const invoiceLines = [{
+      schemeCode: '80001',
+      fundCode: 'DRD10',
+      agreementNumber: undefined,
+      description: 'G00',
+      value: 10
+    }, {
+      schemeCode: '80001',
+      fundCode: 'DRD10',
+      agreementNumber: undefined,
+      description: 'G00',
+      value: -8
+    }]
+
+    const lineDeltas = calculateLineDeltas(invoiceLines)
+    expect(lineDeltas.find(x => x.schemeCode === '80001').value).toBe(2)
+  })
+
+  test('should calculate delta values by group when multiple groups and agreementNumber is undefined', () => {
+    const invoiceLines = [{
+      schemeCode: '80001',
+      fundCode: 'DRD10',
+      agreementNumber: undefined,
+      description: 'G00',
+      value: 10
+    }, {
+      schemeCode: '80001',
+      fundCode: 'DRD10',
+      agreementNumber: undefined,
+      description: 'G00',
+      value: -8
+    }, {
+      schemeCode: '80002',
+      fundCode: 'DRD10',
+      agreementNumber: undefined,
+      description: 'G00',
+      value: 11
+    }, {
+      schemeCode: '80002',
+      fundCode: 'DRD10',
+      agreementNumber: undefined,
       description: 'G00',
       value: -7
     }]
