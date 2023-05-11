157057aa730d3488926be2f694ba96f6da155901
John Watson <johnwatson484@hotmail.com>
2023-05-11 21:46:09 +0100

    Refactor

--- app/processing/process-payment-requests.js
+++ app/processing/process-payment-requests.js
@@ -1,50 +1,11 @@
 const { getPaymentRequests } = require('./get-payment-requests')
-const { transformPaymentRequest } = require('./transform-payment-request')
-const { applyAutoHold } = require('./apply-auto-hold')
-const { requiresDebtData } = require('./requires-debt-data')
-const { routeDebtToRequestEditor } = require('./route-debt-to-request-editor')
-const { requiresManualLedgerCheck } = require('./requires-manual-ledger-check')
-const { routeManualLedgerToRequestEditor } = require('./route-manual-ledger-to-request-editor')
-const { mapAccountCodes } = require('./account-codes/map-account-codes')
-const { completePaymentRequests } = require('./complete-payment-requests')
-
-const { sendProcessingRouteEvent } = require('../event')
+const { processPaymentRequest } = require('./process-payment-request')
 
 const processPaymentRequests = async () => {
   const scheduledPaymentRequests = await getPaymentRequests()
   for (const scheduledPaymentRequest of scheduledPaymentRequests) {
     await processPaymentRequest(scheduledPaymentRequest)
   }
-}
-
-const processPaymentRequest = async (scheduledPaymentRequest) => {
-  const { scheduleId, paymentRequest } = scheduledPaymentRequest
-  const paymentRequests = await transformPaymentRequest(paymentRequest)
-  const { deltaPaymentRequest, completedPaymentRequests } = paymentRequests
-  if (await applyAutoHold(completedPaymentRequests)) {
-    return
-  }
-  // If has AR but no debt enrichment data, then route to request editor and apply hold
-  if (requiresDebtData(completedPaymentRequests)) {
-    await sendProcessingRouteEvent(paymentRequest, 'debt', 'request')
-    await routeDebtToRequestEditor(paymentRequest)
-    return
-  }
-
-  if (deltaPaymentRequest) {
-    const sendToManualLedgerCheck = await requiresManualLedgerCheck(deltaPaymentRequest)
-
-    if (sendToManualLedgerCheck) {
-      await sendProcessingRouteEvent(paymentRequest, 'manual-ledger', 'request')
-      await routeManualLedgerToRequestEditor(paymentRequests)
-      return
-    }
-  }
-
-  for (const completedPaymentRequest of completedPaymentRequests) {
-    await mapAccountCodes(completedPaymentRequest)
-  }
-  await completePaymentRequests(scheduleId, completedPaymentRequests)
 }
 
 module.exports = {
