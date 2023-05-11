951818152567abd3027407c26c82a8b0da339eea
John Watson <johnwatson484@hotmail.com>
2023-05-11 20:50:09 +0100

    Refactor

--- app/reset/index.js
+++ app/reset/index.js
@@ -1,19 +1,5 @@
-const db = require('../data')
-const { sendResetEvent } = require('../event')
 const { resetPaymentRequestById } = require('./reset-payment-request-id')
-
-const resetPaymentRequestByInvoiceNumber = async (invoiceNumber, transaction) => {
-  const paymentRequest = await db.paymentRequest.findOne({ where: { invoiceNumber } }, { transaction })
-  if (!paymentRequest) {
-    throw new Error(`Payment request ${invoiceNumber} does not exist`)
-  }
-  const completedPaymentRequest = await db.completedPaymentRequest.findOne({ where: { paymentRequestId: paymentRequest.paymentRequestId, invalid: false } }, { transaction })
-  if (!completedPaymentRequest) {
-    throw new Error(`Payment request ${invoiceNumber} has not completed processing so cannot be reset`)
-  }
-  await resetPaymentRequestById(paymentRequest.paymentRequestId, paymentRequest.schemeId, transaction)
-  await sendResetEvent(completedPaymentRequest)
-}
+const { resetPaymentRequestByInvoiceNumber } = require('./reset-payment-requests-invoice-number')
 
 module.exports = {
   resetPaymentRequestById,
---
+++ app/reset/reset-payment-requests-invoice-number.js
@@ -0,0 +1,20 @@
+const db = require('../data')
+const { sendResetEvent } = require('../event')
+const { resetPaymentRequestById } = require('./reset-payment-request-id')
+
+const resetPaymentRequestByInvoiceNumber = async (invoiceNumber, transaction) => {
+  const paymentRequest = await db.paymentRequest.findOne({ where: { invoiceNumber } }, { transaction })
+  if (!paymentRequest) {
+    throw new Error(`Payment request ${invoiceNumber} does not exist`)
+  }
+  const completedPaymentRequest = await db.completedPaymentRequest.findOne({ where: { paymentRequestId: paymentRequest.paymentRequestId, invalid: false } }, { transaction })
+  if (!completedPaymentRequest) {
+    throw new Error(`Payment request ${invoiceNumber} has not completed processing so cannot be reset`)
+  }
+  await resetPaymentRequestById(paymentRequest.paymentRequestId, paymentRequest.schemeId, transaction)
+  await sendResetEvent(completedPaymentRequest)
+}
+
+module.exports = {
+  resetPaymentRequestByInvoiceNumber
+}
