afbc975c7e8ffe95e0c59a1853378f94b98c6d85
John Watson <johnwatson484@hotmail.com>
2023-05-14 18:03:31 +0100

    Move tests

--- test/unit/acknowledgement/process-invalid.test.js
+++ test/unit/acknowledgement/process-invalid.test.js
@@ -14,7 +14,7 @@ jest.mock('../../../app/data', () => {
       transaction: mockTransaction
     }
   }
-}) 
+})
 
 jest.mock('../../../app/reset')
 const { resetPaymentRequestById: mockResetPaymentRequestById } = require('../../../app/reset')
