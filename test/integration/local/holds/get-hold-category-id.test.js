49ae5db65f0ee8d53c08bf46c695ea6214ee12db
John Watson <johnwatson484@hotmail.com>
2023-05-14 19:33:09 +0100

    Add tests

---
+++ test/integration/local/holds/get-hold-category-id.test.js
@@ -0,0 +1,31 @@
+const { resetDatabase, closeDatabaseConnection } = require('../../../helpers')
+
+const holdCategory = require('../../../mocks/holds/hold-category')
+
+const { getHoldCategoryId } = require('../../../../app/holds/get-hold-category-id')
+
+describe('get hold categories', () => {
+  beforeEach(async () => {
+    jest.clearAllMocks()
+    await resetDatabase()
+  })
+
+  test('should return hold category id if scheme and name exists', async () => {
+    const holdCategoryId = await getHoldCategoryId(holdCategory.schemeId, holdCategory.name)
+    expect(holdCategoryId).toBe(holdCategory.holdCategoryId)
+  })
+
+  test('should return undefined if scheme does not exist', async () => {
+    const holdCategoryId = await getHoldCategoryId(999, holdCategory.name)
+    expect(holdCategoryId).toBeUndefined()
+  })
+
+  test('should return undefined if name does not exist', async () => {
+    const holdCategoryId = await getHoldCategoryId(holdCategory.schemeId, 'unknown')
+    expect(holdCategoryId).toBeUndefined()
+  })
+
+  afterAll(async () => {
+    await closeDatabaseConnection()
+  })
+})
