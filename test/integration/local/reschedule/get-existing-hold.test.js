1ffc66c15eeabff7227eb1392145b61ea6468abd
John Watson <john.watson1@defra.gov.uk>
2023-05-15 13:31:41 +0100

    Add test

---
+++ test/integration/local/reschedule/get-existing-hold.test.js
@@ -0,0 +1,43 @@
+const { resetDatabase, closeDatabaseConnection } = require('../../../helpers')
+
+const hold = require('../../../mocks/holds/hold')
+const holdCategory = require('../../../mocks/holds/hold-category')
+const { FRN } = require('../../../mocks/values/frn')
+const { TIMESTAMP } = require('../../../mocks/values/date')
+
+const db = require('../../../../app/data')
+
+const { getExistingHold } = require('../../../../app/reschedule/get-existing-hold')
+
+describe('get existing hold', () => {
+  beforeEach(async () => {
+    jest.clearAllMocks()
+    await resetDatabase()
+    await db.hold.create(hold)
+  })
+
+  test('should get existing hold if matched by hold category and FRN', async () => {
+    const existingHold = await getExistingHold(holdCategory.holdCategoryId, FRN)
+    expect(existingHold.frn).toBe(hold.frn.toString())
+  })
+
+  test('should not get existing hold if not matched by hold category', async () => {
+    const existingHold = await getExistingHold(999, FRN)
+    expect(existingHold).toBeNull()
+  })
+
+  test('should not get existing hold if not matched by FRN', async () => {
+    const existingHold = await getExistingHold(holdCategory.holdCategoryId, '9999999999')
+    expect(existingHold).toBeNull()
+  })
+
+  test('should not get closed holds', async () => {
+    await db.hold.update({ closed: TIMESTAMP }, { where: { holdId: hold.holdId } })
+    const existingHold = await getExistingHold(holdCategory.holdCategoryId, FRN)
+    expect(existingHold).toBeNull()
+  })
+
+  afterAll(async () => {
+    await closeDatabaseConnection()
+  })
+})
