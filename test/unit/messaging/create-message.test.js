3b8ceeb31ddf7c77a47448896aa0018f18037751
John Watson <john.watson1@defra.gov.uk>
2023-05-15 10:48:17 +0100

    Add tests

--- test/unit/messaging/create-message.test.js
+++ test/unit/messaging/create-message.test.js
@@ -6,17 +6,17 @@ const { PROCESSING } = require('../../../app/constants/messages')
 const { createMessage } = require('../../../app/messaging/create-message')
 
 describe('create message', () => {
-  test('should create a message with the correct body', () => {
+  test('should create a message with the supplied body', () => {
     const message = createMessage(paymentRequest, PROCESSING)
     expect(message.body).toEqual(paymentRequest)
   })
 
-  test('should create a message with the correct type', () => {
+  test('should create a message with the supplied type', () => {
     const message = createMessage(paymentRequest, PROCESSING)
     expect(message.type).toEqual(PROCESSING)
   })
 
-  test('should create a message with the correct source', () => {
+  test('should create a message with source', () => {
     const message = createMessage(paymentRequest, PROCESSING)
     expect(message.source).toEqual(SOURCE)
   })
