79c588174115da3823cdf08a45ff12f8e2132368
John Watson <john.watson1@defra.gov.uk>
2023-05-17 14:40:48 +0100

    Add tests

--- app/routing/transform-payment-request.js
+++ app/routing/transform-payment-request.js
@@ -1,6 +1,6 @@
-const { enrichPaymentRequests } = require('../processing/enrichment')
-const { confirmDueDates } = require('../processing/due-dates')
 const { getCompletedPaymentRequests } = require('../processing/get-completed-payment-requests')
+const { confirmDueDates } = require('../processing/due-dates')
+const { enrichPaymentRequests } = require('../processing/enrichment')
 
 const transformPaymentRequest = async (paymentRequest, paymentRequests) => {
   const previousPaymentRequests = await getCompletedPaymentRequests(paymentRequest)
