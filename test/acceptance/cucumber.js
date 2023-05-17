9f12a6cd8f35bf1f290f81139a8bb2da87b58afd
Edirin Atumah <96245169+edi-defra@users.noreply.github.com>
2023-05-17 09:45:11 +0100

    Update cucumber config file

--- test/acceptance/cucumber.js
+++ test/acceptance/cucumber.js
@@ -5,7 +5,7 @@ module.exports = {
     snippetInterface: 'async-await',
     paths: ['test/acceptance/features/**/*.feature'],
     backtrace: true,
-    require: ['test/acceptance/step_definitions/**/*.js'],
+    require: ['test/acceptance/step-definitions/**/*.js'],
     parallel: 1,
     format: ['html:test-output/cucumber-report.html']
   }
