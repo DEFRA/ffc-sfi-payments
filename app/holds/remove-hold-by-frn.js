c06ee6a68e16ba77269a0ecd03fc4693183e251e
John Watson <john.watson1@defra.gov.uk>
2023-05-17 13:30:01 +0100

    Add tests

--- app/holds/remove-hold-by-frn.js
+++ app/holds/remove-hold-by-frn.js
@@ -1,9 +1,16 @@
 const db = require('../data')
 const { getHoldCategoryId } = require('.')
+const { sendHoldEvent } = require('../event')
+const { REMOVED } = require('../constants/hold-statuses')
 
 const removeHoldByFrn = async (schemeId, frn, holdCategoryName) => {
   const holdCategoryId = await getHoldCategoryId(schemeId, holdCategoryName)
-  await db.hold.update({ closed: new Date() }, { where: { frn, holdCategoryId } })
+  const hold = await db.hold.findOne({ where: { frn, holdCategoryId, closed: null }, raw: true })
+  if (hold) {
+    const holdClosed = new Date()
+    await db.hold.update({ closed: holdClosed }, { where: { frn, holdCategoryId, closed: null } })
+    await sendHoldEvent({ ...hold, closed: holdClosed }, REMOVED)
+  }
 }
 
 module.exports = {
--- test/integration/local/holds/remove-hold-by-frn.test.js
+++ test/integration/local/holds/remove-hold-by-frn.test.js
@@ -5,30 +5,37 @@ const { sendHoldEvent: mockSendHoldEvent } = require('../../../../app/event')
 
 const { REMOVED } = require('../../../../app/constants/hold-statuses')
 
+const holdCategory = require('../../../mocks/holds/hold-category')
 const hold = require('../../../mocks/holds/hold')
 
 const db = require('../../../../app/data')
 
-const { removeHoldById } = require('../../../../app/holds/remove-hold-by-id')
+const { removeHoldByFrn } = require('../../../../app/holds/remove-hold-by-frn')
 
-describe('remove hold by id', () => {
+describe('remove hold by frn', () => {
   beforeEach(async () => {
     jest.clearAllMocks()
     await resetDatabase()
     await db.hold.create(hold)
   })
 
-  test('should update hold with closed date if hold found', async () => {
-    await removeHoldById(hold.holdId)
+  test('should update hold with closed date', async () => {
+    await removeHoldByFrn(holdCategory.schemeId, hold.frn, holdCategory.name)
     const updatedHold = await db.hold.findOne({ where: { holdId: hold.holdId } })
     expect(updatedHold.closed).not.toBeNull()
   })
 
-  test('should send hold removed event with hold data if hold found', async () => {
-    await removeHoldById(hold.holdId)
+  test('should send hold removed event with hold data if hold exists', async () => {
+    await removeHoldByFrn(holdCategory.schemeId, hold.frn, holdCategory.name)
     const updatedHold = await db.hold.findOne({ where: { holdId: hold.holdId } })
     const plainHold = updatedHold.get({ plain: true })
     expect(mockSendHoldEvent).toHaveBeenCalledWith(plainHold, REMOVED)
+  })
+
+  test('should not send hold removed event if open hold does not exist', async () => {
+    await resetDatabase()
+    await removeHoldByFrn(holdCategory.schemeId, hold.frn, holdCategory.name)
+    expect(mockSendHoldEvent).not.toHaveBeenCalled()
   })
 
   afterAll(async () => {
