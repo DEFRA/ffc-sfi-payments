44f380144c0c38ddc8b1246720e2b252dcb5b5fd
John Watson <john.watson1@defra.gov.uk>
2023-05-17 11:47:22 +0100

    Add tests

---
+++ test/integration/local/reset/reset-payment-request-invoice-number.test.js
@@ -0,0 +1,46 @@
+const { resetDatabase, closeDatabaseConnection, savePaymentRequest } = require('../../../helpers')
+
+jest.mock('../../../../app/event')
+const { sendResetEvent: mockSendResetEvent } = require('../../../../app/event')
+
+jest.mock('../../../../app/reset/reset-reference-id')
+const { resetReferenceId: mockResetReferenceId } = require('../../../../app/reset/reset-reference-id')
+
+const paymentRequest = require('../../../mocks/payment-requests/payment-request')
+
+const db = require('../../../../app/data')
+
+const { resetPaymentRequestByInvoiceNumber } = require('../../../../app/reset/reset-payment-requests-invoice-number')
+
+describe('reset payment requests by invoice number', () => {
+  beforeEach(async () => {
+    jest.clearAllMocks()
+    await resetDatabase()
+  })
+
+  test('should reset payment requests if matched by invoice number and payment has completed processing', async () => {
+    await savePaymentRequest(paymentRequest, true)
+    await resetPaymentRequestByInvoiceNumber(paymentRequest.invoiceNumber)
+    expect(mockResetReferenceId).toHaveBeenCalled()
+  })
+
+  test('should throw error if matched by invoice number but payment has not completed processing', async () => {
+    await savePaymentRequest(paymentRequest, false)
+    await expect(resetPaymentRequestByInvoiceNumber(paymentRequest.invoiceNumber)).rejects.toThrow(`Payment request ${paymentRequest.invoiceNumber} has not completed processing so cannot be reset`)
+  })
+
+  test('should throw error if not matched by invoice number', async () => {
+    await savePaymentRequest(paymentRequest, true)
+    await expect(resetPaymentRequestByInvoiceNumber('999')).rejects.toThrow('Payment request 999 does not exist')
+  })
+
+  test('should send reset event if matched by invoice number and payment has completed processing', async () => {
+    await savePaymentRequest(paymentRequest, true)
+    await resetPaymentRequestByInvoiceNumber(paymentRequest.invoiceNumber)
+    expect(mockSendResetEvent).toHaveBeenCalled()
+  })
+
+  afterAll(async () => {
+    await closeDatabaseConnection()
+  })
+})
