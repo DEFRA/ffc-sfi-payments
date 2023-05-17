4bbccd10f528fcca3667af1ba773922b0cb86ef4
John Watson <john.watson1@defra.gov.uk>
2023-05-17 15:21:36 +0100

    Add tests

--- test/integration/local/routing/update-requests-awaiting-debt-data.test.js
+++ test/integration/local/routing/update-requests-awaiting-debt-data.test.js
@@ -3,25 +3,38 @@ const { resetDatabase, closeDatabaseConnection, savePaymentRequest } = require('
 jest.mock('../../../../app/routing/prepare-for-reprocessing')
 const { prepareForReprocessing: mockPrepareForReprocessing } = require('../../../../app/routing/prepare-for-reprocessing')
 
-const paymentRequest = require('../../../mocks/payment-requests/payment-request')
 const { RECOVERY_DATE } = require('../../../mocks/values/recovery-date')
 
 const { ADMINISTRATIVE } = require('../../../../app/constants/debt-types')
 
 const { updateRequestsAwaitingDebtData } = require('../../../../app/routing/update-requests-awaiting-debt-data')
 
+let paymentRequest
+
 describe('update requests awaiting debt data', () => {
   beforeEach(async () => {
     jest.clearAllMocks()
+    paymentRequest = JSON.parse(JSON.stringify(require('../../../mocks/payment-requests/payment-request')))
     await resetDatabase()
-    await savePaymentRequest(paymentRequest)
   })
 
   test('should prepare payment request for reprocessing if payment request has debt type and can be matched by invoice number', async () => {
+    await savePaymentRequest(paymentRequest)
     paymentRequest.debtType = ADMINISTRATIVE
     paymentRequest.recoveryDate = RECOVERY_DATE
     await updateRequestsAwaitingDebtData(paymentRequest)
     expect(mockPrepareForReprocessing).toHaveBeenCalledWith(expect.any(Object, { invoiceNumber: paymentRequest.invoiceNumber }), ADMINISTRATIVE, RECOVERY_DATE)
+  })
+
+  test('should throw error if received payment request does not have debt data', async () => {
+    await savePaymentRequest(paymentRequest)
+    await expect(async () => updateRequestsAwaitingDebtData(paymentRequest)).rejects.toThrow()
+  })
+
+  test('should throw error if no matching payment request found', async () => {
+    paymentRequest.debtType = ADMINISTRATIVE
+    paymentRequest.recoveryDate = RECOVERY_DATE
+    await expect(async () => updateRequestsAwaitingDebtData(paymentRequest)).rejects.toThrow()
   })
 
   afterAll(async () => {
