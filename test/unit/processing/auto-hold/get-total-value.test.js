35d2e9288b2e6503ae4524bfa080739016d54067
John Watson <john.watson1@defra.gov.uk>
2023-05-19 11:16:19 +0100

    Add tests

---
+++ test/unit/processing/auto-hold/get-total-value.test.js
@@ -0,0 +1,32 @@
+const { getTotalValue } = require('../../../../app/processing/auto-hold/get-total-value')
+
+const paymentRequests = [{
+  value: 1
+}, {
+  value: 2
+}, {
+  value: 3
+}]
+
+describe('get total value', () => {
+  test('should return 0 if no payment requests', () => {
+    const result = getTotalValue([])
+    expect(result).toBe(0)
+  })
+
+  test('should return total value of payment requests', () => {
+    const result = getTotalValue(paymentRequests)
+    expect(result).toBe(6)
+  })
+
+  test('should return total value of payment requests if decimal', () => {
+    const result = getTotalValue([{
+      value: 1.5
+    }, {
+      value: 2.5
+    }, {
+      value: 3.5
+    }])
+    expect(result).toBe(7.5)
+  })
+})
