9c7b8d06f8304844db6d19507bf0ad17b4277412
John Watson <john.watson1@defra.gov.uk>
2023-05-19 16:54:41 +0100

    Add tests

---
+++ test/unit/processing/enrichment/get-original-settlement-date.test.js
@@ -0,0 +1,36 @@
+const { getOriginalSettlementDate } = require('../../../../app/processing/enrichment/get-original-settlement-date')
+
+let paymentRequest
+let paymentRequests
+
+describe('get original settlement date', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+
+    paymentRequest = JSON.parse(JSON.stringify(require('../../../mocks/payment-requests/payment-request')))
+    paymentRequest.lastSettled = '01/01/2023'
+    paymentRequests = [paymentRequest]
+  })
+
+  test('should return undefined if no previous payment requests', () => {
+    expect(getOriginalSettlementDate([])).toBeUndefined()
+  })
+
+  test('should get settlement date of only payment request', () => {
+    expect(getOriginalSettlementDate(paymentRequests)).toEqual(paymentRequest.settlementDate)
+  })
+
+  test('should get earliest settlement date if multiple payment requests and earliest settled first', () => {
+    paymentRequests.push(JSON.parse(JSON.stringify(paymentRequest)))
+    paymentRequests[1].paymentRequestNumber = 2
+    paymentRequests[1].lastSettled = '01/01/2024'
+    expect(getOriginalSettlementDate(paymentRequests)).toEqual(paymentRequests[0].settlementDate)
+  })
+
+  test('should get earliest settlement date if multiple payment requests and earliest settled last', () => {
+    paymentRequests.push(JSON.parse(JSON.stringify(paymentRequest)))
+    paymentRequests[1].paymentRequestNumber = 2
+    paymentRequests[1].lastSettled = '01/01/2022'
+    expect(getOriginalSettlementDate(paymentRequests)).toEqual(paymentRequests[1].settlementDate)
+  })
+})
