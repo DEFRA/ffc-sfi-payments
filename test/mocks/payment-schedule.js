63a4a4eefbfc1228c9f57ec49c86fda4854069bc
John Watson <john.watson1@defra.gov.uk>
2023-05-19 12:11:37 +0100

    Add tests

---
+++ test/mocks/payment-schedule.js
@@ -0,0 +1,13 @@
+module.exports = [{
+  dueDate: '05/04/2023',
+  outstanding: false
+}, {
+  dueDate: '05/07/2023',
+  outstanding: false
+}, {
+  dueDate: '05/10/2023',
+  outstanding: false
+}, {
+  dueDate: '05/01/2024',
+  outstanding: false
+}]
--- test/unit/processing/due-dates/confirm-due-dates.test.js
+++ test/unit/processing/due-dates/confirm-due-dates.test.js
@@ -9,9 +9,15 @@ const { getTotalValue: mockGetTotalValue } = require('../../../../app/processing
 jest.mock('../../../../app/processing/due-dates/get-payment-schedule')
 const { getPaymentSchedule: mockGetPaymentSchedule } = require('../../../../app/processing/due-dates/get-payment-schedule')
 
-const { TOP_UP } = require('../../../../app/constants/adjustment-types')
+const paymentSchedule = require('../../../mocks/payment-schedule')
+
+const { RECOVERY } = require('../../../../app/constants/adjustment-types')
+const { AR } = require('../../../../app/constants/ledgers')
 
 const { confirmDueDates } = require('../../../../app/processing/due-dates/confirm-due-dates')
+const { Q1 } = require('../../../../app/constants/schedules')
+
+const settledValue = 0
 
 let previousPaymentRequest
 let previousPaymentRequests
@@ -23,8 +29,12 @@ describe('confirm due dates', () => {
     jest.clearAllMocks()
     previousPaymentRequest = JSON.parse(JSON.stringify(require('../../../mocks/payment-requests/payment-request')))
     previousPaymentRequests = [previousPaymentRequest]
-    paymentRequest = createAdjustmentPaymentRequest(previousPaymentRequest, TOP_UP)
+    paymentRequest = createAdjustmentPaymentRequest(previousPaymentRequest, RECOVERY)
     paymentRequests = [paymentRequest]
+
+    mockGetSettledValue.mockReturnValue(settledValue)
+    mockGetTotalValue.mockReturnValue(previousPaymentRequest.value)
+    mockGetPaymentSchedule.mockReturnValue(paymentSchedule)
   })
 
   test('should not recalculate due dates if first payment request does not have a schedule', async () => {
@@ -37,5 +47,57 @@ describe('confirm due dates', () => {
     delete previousPaymentRequest.schedule
     const result = await confirmDueDates(paymentRequests, previousPaymentRequests)
     expect(result).toEqual(paymentRequests)
+  })
+
+  test('should not recalculate due dates if no first payment request', async () => {
+    previousPaymentRequest.paymentRequestNumber = 2
+    await confirmDueDates(paymentRequests, previousPaymentRequests)
+    expect(mockGetSettledValue).not.toHaveBeenCalled()
+  })
+
+  test('should return payment requests unchanged if no first payment request', async () => {
+    previousPaymentRequest.paymentRequestNumber = 2
+    const result = await confirmDueDates(paymentRequests, previousPaymentRequests)
+    expect(result).toEqual(paymentRequests)
+  })
+
+  test('should calculate total settled of all previous payment requests', async () => {
+    await confirmDueDates(paymentRequests, previousPaymentRequests)
+    expect(mockGetSettledValue).toHaveBeenCalledWith(previousPaymentRequests)
+  })
+
+  test('should calculate total value of all previous payment requests', async () => {
+    await confirmDueDates(paymentRequests, previousPaymentRequests)
+    expect(mockGetTotalValue).toHaveBeenCalledWith(previousPaymentRequests)
+  })
+
+  test('should calculate payment schedule', async () => {
+    await confirmDueDates(paymentRequests, previousPaymentRequests)
+    expect(mockGetPaymentSchedule).toHaveBeenCalledWith(previousPaymentRequest.schedule, previousPaymentRequest.dueDate, settledValue, previousPaymentRequest.value, expect.any(Date))
+  })
+
+  test('should return payment requests unchanged if no outstanding payments', async () => {
+    const result = await confirmDueDates(paymentRequests, previousPaymentRequests)
+    expect(result).toEqual(paymentRequests)
+  })
+
+  test('should return payment requests unchanged if outstanding payments but all requests AR', async () => {
+    paymentSchedule[3].outstanding = true
+    paymentRequests[0].ledger = AR
+    const result = await confirmDueDates(paymentRequests, previousPaymentRequests)
+    expect(result).toEqual(paymentRequests)
+  })
+
+  test('should return payment requests unchanged if outstanding payments but all values positive', async () => {
+    paymentSchedule[3].outstanding = true
+    paymentRequests[0].value = 1
+    const result = await confirmDueDates(paymentRequests, previousPaymentRequests)
+    expect(result).toEqual(paymentRequests)
+  })
+
+  test('should reduce remaining schedule to one of only one instalment remaining', async () => {
+    paymentSchedule[3].outstanding = true
+    const result = await confirmDueDates(paymentRequests, previousPaymentRequests)
+    expect(result[0].schedule).toBe(Q1)
   })
 })
