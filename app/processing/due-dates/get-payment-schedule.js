b64c57b90f54f1b9e010a375a82326df117994ae
John Watson <john.watson1@defra.gov.uk>
2023-05-19 12:31:41 +0100

    Add tests

--- app/processing/due-dates/get-payment-schedule.js
+++ app/processing/due-dates/get-payment-schedule.js
@@ -1,17 +1,18 @@
 const moment = require('moment')
 const { getSchedule } = require('./get-schedule')
 const { Q4, M12, T4 } = require('../../constants/schedules')
+const { MONTH, DAY } = require('../../constants/time-periods')
 
 const getPaymentSchedule = (schedule, dueDate, settledValue, totalValue, currentDate) => {
   const scheduleDate = moment(dueDate, 'DD/MM/YYYY')
 
   switch (schedule) {
     case Q4:
-      return getSchedule(scheduleDate, 4, settledValue, totalValue, 3, 'month', currentDate)
+      return getSchedule(scheduleDate, 4, settledValue, totalValue, 3, MONTH, currentDate)
     case M12:
-      return getSchedule(scheduleDate, 12, settledValue, totalValue, 1, 'month', currentDate)
+      return getSchedule(scheduleDate, 12, settledValue, totalValue, 1, MONTH, currentDate)
     case T4:
-      return getSchedule(scheduleDate, 4, settledValue, totalValue, 3, 'day', currentDate)
+      return getSchedule(scheduleDate, 4, settledValue, totalValue, 3, DAY, currentDate)
     default:
       throw new Error(`Unknown schedule ${schedule}`)
   }
