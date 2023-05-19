2c1c81404c6819ca149ef830dce8817fcf6e75b8
John Watson <john.watson1@defra.gov.uk>
2023-05-19 16:51:25 +0100

    Add tests

--- app/processing/enrichment/get-original-settlement-date.js
+++ app/processing/enrichment/get-original-settlement-date.js
@@ -4,7 +4,6 @@ const { convertDateToDDMMYYYY } = require('../../convert-date')
 const getOriginalSettlementDate = (paymentRequests) => {
   // first settlement date of agreement on AP ledger
   const settlementDates = paymentRequests
-    // sequelize returns empty values as null so need null check
     .filter(x => x.ledger === AP && x.lastSettlement != null)
     .map(x => x.lastSettlement)
 
