e9a800e7fe85a57c47b211a1a9d8ceb101976b3c
John Watson <johnwatson484@hotmail.com>
2023-05-21 16:58:24 +0100

    Add tests

--- app/processing/delta/get-default-agreement-number.js
+++ app/processing/delta/get-default-agreement-number.js
@@ -1,6 +1,17 @@
 const getDefaultAgreementNumber = (paymentRequest, previousPaymentRequests) => {
   const firstAgreementNumber = previousPaymentRequests.filter(x => x.agreementNumber)?.sort((a, b) => a.paymentRequestNumber - b.paymentRequestNumber)[0]?.agreementNumber
-  return firstAgreementNumber ?? paymentRequest.agreementNumber ?? paymentRequest.invoiceLines.filter(x => x.agreementNumber)[0]?.agreementNumber
+  if (firstAgreementNumber) {
+    return firstAgreementNumber
+  }
+  const previousAgreementNumbers = []
+  for (const previousPaymentRequest of previousPaymentRequests) {
+    for (const previousInvoiceLine of previousPaymentRequest.invoiceLines) {
+      if (previousInvoiceLine.agreementNumber) {
+        previousAgreementNumbers.push(previousInvoiceLine.agreementNumber)
+      }
+    }
+  }
+  return previousAgreementNumbers[0] ?? paymentRequest.agreementNumber
 }
 
 module.exports = {
--- test/unit/processing/delta/get-default-agreement-number.test.js
+++ test/unit/processing/delta/get-default-agreement-number.test.js
@@ -1,5 +1,7 @@
 const { getDefaultAgreementNumber } = require('../../../../app/processing/delta/get-default-agreement-number')
 
+const agreementNumber = '12345678'
+
 let paymentRequest
 let previousPaymentRequests
 
@@ -8,11 +10,16 @@ describe('get default agreement number', () => {
     jest.clearAllMocks()
 
     paymentRequest = JSON.parse(JSON.stringify(require('../../../mocks/payment-requests/payment-request')))
-    previousPaymentRequests = [paymentRequest]
+    previousPaymentRequests = [{ ...paymentRequest, agreementNumber }]
   })
 
   test('should return current agreement number if no previous payment requests', () => {
     const result = getDefaultAgreementNumber(paymentRequest, [])
+    expect(result).toEqual(paymentRequest.agreementNumber)
+  })
+
+  test('should return first agreement number if previous payment requests', () => {
+    const result = getDefaultAgreementNumber(paymentRequest, previousPaymentRequests)
     expect(result).toEqual(paymentRequest.agreementNumber)
   })
 })
