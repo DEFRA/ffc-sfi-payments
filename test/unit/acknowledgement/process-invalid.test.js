831801d3d6aac86725dfae2d308493d2853c757b
John Watson <john.watson1@defra.gov.uk>
2023-05-24 11:33:04 +0100

    Feedback

--- test/unit/acknowledgement/process-invalid.test.js
+++ test/unit/acknowledgement/process-invalid.test.js
@@ -37,10 +37,10 @@ const { FRN } = require('../../mocks/values/frn')
 const { DAX_REJECTION } = require('../../../app/constants/hold-categories-names')
 const { SFI } = require('../../../app/constants/schemes')
 
+const { processInvalid } = require('../../../app/acknowledgement/process-invalid')
+
 const PAYMENT_REQUEST_ID = 1
 const HOLD_CATEGORY_ID = 1
-
-const { processInvalid } = require('../../../app/acknowledgement/process-invalid')
 
 describe('process invalid acknowledgement', () => {
   beforeEach(() => {
