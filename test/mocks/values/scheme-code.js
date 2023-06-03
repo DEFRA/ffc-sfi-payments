b97cc8ed3fbeee824b11800cbcdc3951197fe997
John Watson <johnwatson484@hotmail.com>
2023-06-03 19:26:00 +0100

    Update measure codes

--- test/mocks/values/scheme-code.js
+++ test/mocks/values/scheme-code.js
@@ -1,7 +1,7 @@
 module.exports = {
   SCHEME_CODE: '80001',
   MEASURE_4_SCHEME_CODE: '5200',
-  MEASURE_8_SCHEME_CODE: '5201', // TODO: This is not correct
-  MEASURE_11_SCHEME_CODE: '5202', // TODO: This is not correct
-  MEASURE_15_SCHEME_CODE: '5203' // TODO: This is not correct
+  MEASURE_8_SCHEME_CODE: '5500',
+  MEASURE_11_SCHEME_CODE: '5800',
+  MEASURE_15_SCHEME_CODE: '5900'
 }
