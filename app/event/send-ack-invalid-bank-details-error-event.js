219042b8032567eaf8c13c636f124bb95c2457b6
Simon Dunn <simondunn90@gmail.com>
2023-06-15 10:41:37 +0100

    remove from send-ack-invalid-bank-details

--- app/event/send-ack-invalid-bank-details-error-event.js
+++ app/event/send-ack-invalid-bank-details-error-event.js
@@ -1,28 +1,12 @@
-const { raiseEvent } = require('./raise-event')
-const { v4: uuidv4 } = require('uuid')
 const { processingConfig, messageConfig } = require('../config')
 const { EventPublisher } = require('ffc-pay-event-publisher')
 const { SOURCE } = require('../constants/source')
 const { PAYMENT_INVALID_BANK } = require('../constants/events')
 
 const sendAckInvalidBankDetailsErrorEvent = async (frn) => {
-  if (processingConfig.useV1Events) {
-    await sendV1AckInvalidBankDetailsErrorEvent(frn)
-  }
   if (processingConfig.useV2Events) {
     await sendV2AckInvalidBankDetailsErrorEvent(frn)
   }
-}
-
-const sendV1AckInvalidBankDetailsErrorEvent = async (frn) => {
-  const event = {
-    id: uuidv4(),
-    name: 'invalid-bank-details',
-    type: 'error',
-    message: 'No valid bank details held',
-    data: { frn }
-  }
-  await raiseEvent(event)
 }
 
 const sendV2AckInvalidBankDetailsErrorEvent = async (frn) => {
