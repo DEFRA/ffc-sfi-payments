55d48e5d40918707b9d80ea4088d86e01666ef0c
Simon Dunn <simondunn90@gmail.com>
2023-06-15 11:45:31 +0100

    remove v1 from send-return events

--- app/event/send-return-event.js
+++ app/event/send-return-event.js
@@ -1,5 +1,3 @@
-const { raiseEvent } = require('./raise-event')
-const { v4: uuidv4 } = require('uuid')
 const { getPaymentRequestByInvoiceAndFrn } = require('../processing/get-payment-request-by-invoice-frn')
 const { processingConfig, messageConfig } = require('../config')
 const { EventPublisher } = require('ffc-pay-event-publisher')
@@ -17,48 +15,16 @@ const sendProcessingReturnEvent = async (message, isError = false) => {
   }
 }
 
-const returnEvent = () => {
-  return {
-    name: 'payment-request-return',
-    type: 'return',
-    message: 'Settlement received from DAX'
-  }
-}
-
 const raiseCompletedReturnEvent = async (invoiceNumber, frn) => {
-  if (processingConfig.useV1Events) {
-    await raiseV1CompletedReturnEvent(invoiceNumber, frn)
-  }
   if (processingConfig.useV2Events) {
     await raiseV2CompletedReturnEvent(invoiceNumber, frn)
   }
 }
 
 const raiseErrorEvent = async (invoiceNumber, frn) => {
-  if (processingConfig.useV1Events) {
-    await raiseV1ErrorEvent(invoiceNumber, frn)
-  }
   if (processingConfig.useV2Events) {
     await raiseV2ErrorEvent(invoiceNumber, frn)
   }
-}
-
-const raiseV1CompletedReturnEvent = async (invoiceNumber, frn) => {
-  const completedPaymentRequest = await getPaymentRequestByInvoiceAndFrn(invoiceNumber, frn)
-  if (completedPaymentRequest) {
-    const { correlationId, paymentRequestNumber, agreementNumber } = completedPaymentRequest
-    const id = correlationId ?? uuidv4()
-    const event = { ...returnEvent(), id, data: { paymentRequestNumber, agreementNumber } }
-    await raiseEvent(event)
-  } else {
-    await raiseErrorEvent(invoiceNumber, frn)
-  }
-}
-
-const raiseV1ErrorEvent = async (invoiceNumber, frn) => {
-  const errorMessage = `Unable to find settlement for payment request ${invoiceNumber} and frn ${frn}`
-  const event = { ...returnEvent(), id: uuidv4(), data: { invoiceNumber, frn, errorMessage }, type: 'error' }
-  await raiseEvent(event)
 }
 
 const raiseV2CompletedReturnEvent = async (invoiceNumber, frn) => {
--- test/unit/event/send-return-event.test.js
+++ test/unit/event/send-return-event.test.js
@@ -35,115 +35,12 @@ beforeEach(() => {
 
   getPaymentRequestByInvoiceAndFrn.mockResolvedValue(paymentRequest)
 
-  processingConfig.useV1Events = true
   processingConfig.useV2Events = true
-  messageConfig.eventTopic = 'v1-events'
   messageConfig.eventsTopic = 'v2-events'
 })
 
 afterEach(() => {
   jest.clearAllMocks()
-})
-
-describe('V1 acknowledgment error event', () => {
-  test('should send V1 event for unmatched settlement if V1 events enabled', async () => {
-    processingConfig.useV1Events = true
-    await sendProcessingReturnEvent(settlement, true)
-    expect(mockSendEvent).toHaveBeenCalled()
-  })
-
-  test('should send V1 event for matched settlement if V1 events enabled', async () => {
-    processingConfig.useV1Events = true
-    await sendProcessingReturnEvent(settlement)
-    expect(mockSendEvent).toHaveBeenCalled()
-  })
-
-  test('should not send V1 event for unmatched settlement if V1 events disabled', async () => {
-    processingConfig.useV1Events = false
-    await sendProcessingReturnEvent(settlement, true)
-    expect(mockSendEvent).not.toHaveBeenCalled()
-  })
-
-  test('should not send V1 event for matched settlement if V1 events disabled', async () => {
-    processingConfig.useV1Events = false
-    await sendProcessingReturnEvent(settlement)
-    expect(mockSendEvent).not.toHaveBeenCalled()
-  })
-
-  test('should send unmatched settlement event to V1 topic', async () => {
-    await sendProcessingReturnEvent(settlement, true)
-    expect(MockPublishEvent.mock.calls[0][0]).toBe(messageConfig.eventTopic)
-  })
-
-  test('should send matched settlement event to V1 topic', async () => {
-    await sendProcessingReturnEvent(settlement)
-    expect(MockPublishEvent.mock.calls[0][0]).toBe(messageConfig.eventTopic)
-  })
-
-  test('should raise a unmatched settlement event with new id', async () => {
-    await sendProcessingReturnEvent(settlement, true)
-    expect(mockSendEvent.mock.calls[0][0].properties.id).toMatch(/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i)
-  })
-
-  test('should raise a matched settlement event with new Id', async () => {
-    await sendProcessingReturnEvent(settlement)
-    expect(mockSendEvent.mock.calls[0][0].properties.id).toMatch(paymentRequest.correlationId)
-  })
-
-  test('should raise unmatched settlement event with payment-request-return event name', async () => {
-    await sendProcessingReturnEvent(settlement, true)
-    expect(mockSendEvent.mock.calls[0][0].name).toBe('payment-request-return')
-  })
-
-  test('should raise matched settlement event with payment-request-return event name', async () => {
-    await sendProcessingReturnEvent(settlement)
-    expect(mockSendEvent.mock.calls[0][0].name).toBe('payment-request-return')
-  })
-
-  test('should raise success status event for unmatched settlement', async () => {
-    await sendProcessingReturnEvent(settlement, true)
-    expect(mockSendEvent.mock.calls[0][0].properties.status).toBe('success')
-  })
-
-  test('should raise success status event for matched settlement', async () => {
-    await sendProcessingReturnEvent(settlement)
-    expect(mockSendEvent.mock.calls[0][0].properties.status).toBe('success')
-  })
-
-  test('should raise error event type for unmatched settlement', async () => {
-    await sendProcessingReturnEvent(settlement, true)
-    expect(mockSendEvent.mock.calls[0][0].properties.action.type).toBe('error')
-  })
-
-  test('should raise error event type for matched settlement', async () => {
-    await sendProcessingReturnEvent(settlement)
-    expect(mockSendEvent.mock.calls[0][0].properties.action.type).toBe('return')
-  })
-
-  test('should raise unmatched settlement message for unmatched settlement', async () => {
-    await sendProcessingReturnEvent(settlement, true)
-    expect(mockSendEvent.mock.calls[0][0].properties.action.message).toBe('Settlement received from DAX')
-  })
-
-  test('should raise no valid bank details message for matched settlement', async () => {
-    await sendProcessingReturnEvent(settlement)
-    expect(mockSendEvent.mock.calls[0][0].properties.action.message).toBe('Settlement received from DAX')
-  })
-
-  test('should include settlement in event data for unmatched settlement', async () => {
-    await sendProcessingReturnEvent(settlement, true)
-    expect(mockSendEvent.mock.calls[0][0].properties.action.data).toMatchObject({ frn: settlement.frn, invoiceNumber: settlement.invoiceNumber })
-  })
-
-  test('should include payment request number in event data for matched settlement', async () => {
-    await sendProcessingReturnEvent(settlement)
-    expect(mockSendEvent.mock.calls[0][0].properties.action.data.paymentRequestNumber).toEqual(paymentRequest.paymentRequestNumber)
-  })
-
-  test('should include agreement number in event data for matched settlement', async () => {
-    await sendProcessingReturnEvent(settlement)
-    expect(mockSendEvent.mock.calls[0][0].properties.action.data.agreementNumber).toEqual(paymentRequest.agreementNumber)
-  })
 })
 
 describe('V2 acknowledgement error event', () => {
