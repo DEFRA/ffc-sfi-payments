6a0f6fbb427ef755712970339d9dd3d7ee7c7bc3
Simon Dunn <simondunn90@gmail.com>
2023-06-15 11:48:12 +0100

    remove from send-route-event

--- test/unit/event/send-route-event.test.js
+++ test/unit/event/send-route-event.test.js
@@ -30,136 +30,23 @@ let routeLocationLedger
 let routeTypeRequest
 let routeTypeResponse
 
-beforeEach(() => {
-  paymentRequest = JSON.parse(JSON.stringify(require('../../mocks/payment-requests/payment-request')))
-
-  routeLocationDebt = 'debt'
-  routeLocationLedger = 'manual-ledger'
-  routeTypeRequest = 'request'
-  routeTypeResponse = 'response'
-
-  processingConfig.useV1Events = true
-  processingConfig.useV2Events = true
-  messageConfig.eventTopic = 'v1-events'
-  messageConfig.eventsTopic = 'v2-events'
-})
-
-afterEach(() => {
-  jest.clearAllMocks()
-})
-
-describe('V1 route event', () => {
-  test('should send V1 event for debt routing if V1 events enabled', async () => {
-    processingConfig.useV1Events = true
-    await sendProcessingRouteEvent(paymentRequest, routeLocationDebt, routeTypeRequest)
-    expect(mockSendEvent).toHaveBeenCalled()
-  })
-
-  test('should send V1 event for ledger routing if V1 events enabled', async () => {
-    processingConfig.useV1Events = true
-    await sendProcessingRouteEvent(paymentRequest, routeLocationLedger, routeTypeRequest)
-    expect(mockSendEvent).toHaveBeenCalled()
-  })
-
-  test('should not send V1 event for debt routing if V1 events disabled', async () => {
-    processingConfig.useV1Events = false
-    await sendProcessingRouteEvent(paymentRequest, routeLocationDebt, routeTypeRequest)
-    expect(mockSendEvent).not.toHaveBeenCalled()
-  })
-
-  test('should not send V1 event for ledger routing if V1 events disabled', async () => {
-    processingConfig.useV1Events = false
-    await sendProcessingRouteEvent(paymentRequest, routeLocationLedger, routeTypeRequest)
-    expect(mockSendEvent).not.toHaveBeenCalled()
-  })
-
-  test('should send debt routing event to V1 topic', async () => {
-    await sendProcessingRouteEvent(paymentRequest, routeLocationDebt, routeTypeRequest)
-    expect(MockPublishEvent.mock.calls[0][0]).toBe(messageConfig.eventTopic)
-  })
-
-  test('should send ledger routing event to V1 topic', async () => {
-    await sendProcessingRouteEvent(paymentRequest, routeLocationLedger, routeTypeRequest)
-    expect(MockPublishEvent.mock.calls[0][0]).toBe(messageConfig.eventTopic)
-  })
-
-  test('should raise a debt routing event with new id', async () => {
-    await sendProcessingRouteEvent(paymentRequest, routeLocationDebt, routeTypeRequest)
-    expect(mockSendEvent.mock.calls[0][0].properties.id).toMatch(/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i)
-  })
-
-  test('should raise a ledger routing event with new Id', async () => {
-    await sendProcessingRouteEvent(paymentRequest, routeLocationLedger, routeTypeRequest)
-    expect(mockSendEvent.mock.calls[0][0].properties.id).toMatch(paymentRequest.correlationId)
-  })
-
-  test('should raise debt routing event with payment-request-debt-request event name if request', async () => {
-    await sendProcessingRouteEvent(paymentRequest, routeLocationDebt, routeTypeRequest)
-    expect(mockSendEvent.mock.calls[0][0].name).toBe('payment-request-debt-request')
-  })
-
-  test('should raise debt routing event with payment-request-debt-response event name if response', async () => {
-    await sendProcessingRouteEvent(paymentRequest, routeLocationDebt, routeTypeResponse)
-    expect(mockSendEvent.mock.calls[0][0].name).toBe('payment-request-debt-response')
-  })
-
-  test('should raise ledger routing event with payment-request-manual-ledger-request event name if request', async () => {
-    await sendProcessingRouteEvent(paymentRequest, routeLocationLedger, routeTypeRequest)
-    expect(mockSendEvent.mock.calls[0][0].name).toBe('payment-request-manual-ledger-request')
-  })
-
-  test('should raise ledger routing event with payment-request-manual-ledger-response event name if response', async () => {
-    await sendProcessingRouteEvent(paymentRequest, routeLocationLedger, routeTypeResponse)
-    expect(mockSendEvent.mock.calls[0][0].name).toBe('payment-request-manual-ledger-response')
-  })
-
-  test('should raise success status event for debt routing', async () => {
-    await sendProcessingRouteEvent(paymentRequest, routeLocationDebt, routeTypeRequest)
-    expect(mockSendEvent.mock.calls[0][0].properties.status).toBe('success')
-  })
-
-  test('should raise success status event for ledger routing', async () => {
-    await sendProcessingRouteEvent(paymentRequest, routeLocationLedger, routeTypeRequest)
-    expect(mockSendEvent.mock.calls[0][0].properties.status).toBe('success')
-  })
-
-  test('should raise error event type for debt routing', async () => {
-    await sendProcessingRouteEvent(paymentRequest, routeLocationDebt, routeTypeRequest)
-    expect(mockSendEvent.mock.calls[0][0].properties.action.type).toBe('info')
-  })
-
-  test('should raise error event type for ledger routing', async () => {
-    await sendProcessingRouteEvent(paymentRequest, routeLocationLedger, routeTypeRequest)
-    expect(mockSendEvent.mock.calls[0][0].properties.action.type).toBe('info')
-  })
-
-  test('should raise debt routing message for debt routing', async () => {
-    await sendProcessingRouteEvent(paymentRequest, routeLocationDebt, routeTypeRequest)
-    expect(mockSendEvent.mock.calls[0][0].properties.action.message).toBe('Payment request routed to request editor')
-  })
-
-  test('should raise no valid bank details message for ledger routing', async () => {
-    await sendProcessingRouteEvent(paymentRequest, routeLocationLedger, routeTypeRequest)
-    expect(mockSendEvent.mock.calls[0][0].properties.action.message).toBe('Payment request routed to request editor')
-  })
-
-  test('should include paymentRequest, routeLocationLedger, routeTypeRequest in event data for debt routing', async () => {
-    await sendProcessingRouteEvent(paymentRequest, routeLocationDebt, routeTypeRequest)
-    expect(mockSendEvent.mock.calls[0][0].properties.action.data).toMatchObject(paymentRequest, routeLocationLedger, routeTypeRequest)
-  })
-
-  test('should include payment request number in event data for ledger routing', async () => {
-    await sendProcessingRouteEvent(paymentRequest, routeLocationLedger, routeTypeRequest)
-    expect(mockSendEvent.mock.calls[0][0].properties.action.data.paymentRequestNumber).toEqual(paymentRequest.paymentRequestNumber)
-  })
-
-  test('should include agreement number in event data for ledger routing', async () => {
-    await sendProcessingRouteEvent(paymentRequest, routeLocationLedger, routeTypeRequest)
-    expect(mockSendEvent.mock.calls[0][0].properties.action.data.agreementNumber).toEqual(paymentRequest.agreementNumber)
-  })
-})
-
 describe('V2 route event', () => {
+  beforeEach(() => {
+    paymentRequest = JSON.parse(JSON.stringify(require('../../mocks/payment-requests/payment-request')))
+
+    routeLocationDebt = 'debt'
+    routeLocationLedger = 'manual-ledger'
+    routeTypeRequest = 'request'
+    routeTypeResponse = 'response'
+
+    processingConfig.useV2Events = true
+    messageConfig.eventsTopic = 'v2-events'
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
   test('should send V2 event for debt routing if V2 events enabled', async () => {
     processingConfig.useV2Events = true
     await sendProcessingRouteEvent(paymentRequest, routeLocationDebt, routeTypeRequest)
