f262f8aca9a301e3038156b26b07d24d4078c198
John Watson <john.watson1@defra.gov.uk>
2023-07-05 14:06:40 +0100

    Add tests

--- test/unit/processing/process-payment-request.test.js
+++ test/unit/processing/process-payment-request.test.js
@@ -7,8 +7,8 @@ const { applyAutoHold: mockApplyAutoHold } = require('../../../app/processing/au
 jest.mock('../../../app/processing/requires-debt-data')
 const { requiresDebtData: mockRequiresDebtData } = require('../../../app/processing/requires-debt-data')
 
-jest.mock('../../../app/processing/routing')
-const { routeDebtToRequestEditor: mockRouteDebtToRequestEditor, routeManualLedgerToRequestEditor: mockRouteManualLedgerToRequestEditor } = require('../../../app/processing/routing')
+jest.mock('../../../app/routing')
+const { routeDebtToRequestEditor: mockRouteDebtToRequestEditor, routeManualLedgerToRequestEditor: mockRouteManualLedgerToRequestEditor } = require('../../../app/routing')
 
 jest.mock('../../../app/processing/requires-manual-ledger-check')
 const { requiresManualLedgerCheck: mockRequiresManualLedgerCheck } = require('../../../app/processing/requires-manual-ledger-check')
@@ -19,10 +19,10 @@ const { mapAccountCodes: mockMapAccountCodes } = require('../../../app/processin
 jest.mock('../../../app/processing/complete-payment-requests')
 const { completePaymentRequests: mockCompletePaymentRequests } = require('../../../app/processing/complete-payment-requests')
 
-jest.mock('../../../app/processing/event')
-const { sendProcessingRouteEvent: mockSendProcessingRouteEvent } = require('../../../app/processing/event')
+jest.mock('../../../app/event')
+const { sendProcessingRouteEvent: mockSendProcessingRouteEvent } = require('../../../app/event')
 
-const { MANUAL, ES, IMPS, FC } = require('../../../app/processing/constants/schemes')
+const { MANUAL, ES, IMPS, FC } = require('../../../app/constants/schemes')
 
 const { processPaymentRequest } = require('../../../app/processing/process-payment-request')
 
@@ -40,10 +40,90 @@ describe('process payment request', () => {
       paymentRequest,
       scheduleId
     }
+
+    mockTransformPaymentRequest.mockResolvedValue({ deltaPaymentRequest: paymentRequest, completedPaymentRequests: [paymentRequest] })
+    mockRequiresDebtData.mockReturnValue(false)
+    mockRequiresManualLedgerCheck.mockResolvedValue(false)
   })
 
   test('manual payments should complete payment request without further processing', async () => {
     paymentRequest.schemeId = MANUAL
+    await processPaymentRequest(scheduledPaymentRequest)
+    expect(mockCompletePaymentRequests).toHaveBeenCalledWith(scheduleId, [paymentRequest])
+    expect(mockTransformPaymentRequest).not.toHaveBeenCalled()
+  })
+
+  test('ES payments should complete payment request without further processing', async () => {
+    paymentRequest.schemeId = ES
+    await processPaymentRequest(scheduledPaymentRequest)
+    expect(mockCompletePaymentRequests).toHaveBeenCalledWith(scheduleId, [paymentRequest])
+    expect(mockTransformPaymentRequest).not.toHaveBeenCalled()
+  })
+
+  test('IMPS payments should complete payment request without further processing', async () => {
+    paymentRequest.schemeId = IMPS
+    await processPaymentRequest(scheduledPaymentRequest)
+    expect(mockCompletePaymentRequests).toHaveBeenCalledWith(scheduleId, [paymentRequest])
+    expect(mockTransformPaymentRequest).not.toHaveBeenCalled()
+  })
+
+  test('FC payments should complete payment request without further processing', async () => {
+    paymentRequest.schemeId = FC
+    await processPaymentRequest(scheduledPaymentRequest)
+    expect(mockCompletePaymentRequests).toHaveBeenCalledWith(scheduleId, [paymentRequest])
+    expect(mockTransformPaymentRequest).not.toHaveBeenCalled()
+  })
+
+  test('should transform payment request', async () => {
+    await processPaymentRequest(scheduledPaymentRequest)
+    expect(mockTransformPaymentRequest).toHaveBeenCalledWith(paymentRequest)
+  })
+
+  test('should check if auto hold should be applied', async () => {
+    await processPaymentRequest(scheduledPaymentRequest)
+    expect(mockApplyAutoHold).toHaveBeenCalledWith([paymentRequest])
+  })
+
+  test('should check if debt data is required', async () => {
+    await processPaymentRequest(scheduledPaymentRequest)
+    expect(mockRequiresDebtData).toHaveBeenCalledWith([paymentRequest])
+  })
+
+  test('should route to debt editor if debt data is required', async () => {
+    mockRequiresDebtData.mockReturnValue(true)
+    await processPaymentRequest(scheduledPaymentRequest)
+    expect(mockRouteDebtToRequestEditor).toHaveBeenCalledWith(paymentRequest)
+  })
+
+  test('should send processing route event if debt data is required', async () => {
+    mockRequiresDebtData.mockReturnValue(true)
+    await processPaymentRequest(scheduledPaymentRequest)
+    expect(mockSendProcessingRouteEvent).toHaveBeenCalledWith(paymentRequest, 'debt', 'request')
+  })
+
+  test('should check if manual ledger check is required', async () => {
+    await processPaymentRequest(scheduledPaymentRequest)
+    expect(mockRequiresManualLedgerCheck).toHaveBeenCalledWith(paymentRequest)
+  })
+
+  test('should route to manual ledger editor if manual ledger check is required', async () => {
+    mockRequiresManualLedgerCheck.mockResolvedValue(true)
+    await processPaymentRequest(scheduledPaymentRequest)
+    expect(mockRouteManualLedgerToRequestEditor).toHaveBeenCalledWith({ deltaPaymentRequest: paymentRequest, completedPaymentRequests: [paymentRequest] })
+  })
+
+  test('should send processing route event if manual ledger check is required', async () => {
+    mockRequiresManualLedgerCheck.mockResolvedValue(true)
+    await processPaymentRequest(scheduledPaymentRequest)
+    expect(mockSendProcessingRouteEvent).toHaveBeenCalledWith(paymentRequest, 'manual-ledger', 'request')
+  })
+
+  test('should map account codes', async () => {
+    await processPaymentRequest(scheduledPaymentRequest)
+    expect(mockMapAccountCodes).toHaveBeenCalledWith(paymentRequest)
+  })
+
+  test('should complete payment request', async () => {
     await processPaymentRequest(scheduledPaymentRequest)
     expect(mockCompletePaymentRequests).toHaveBeenCalledWith(scheduleId, [paymentRequest])
   })
