745ab50b2cf5f33128afeaa57f660ebe335327de
Sam Plackett <samuel.plackett@eviden.com>
2023-08-17 16:44:29 +0100

    add SFI23 for payment holds

--- app/processing/delta/invoice-number/create-split-invoice-number.js
+++ app/processing/delta/invoice-number/create-split-invoice-number.js
@@ -1,4 +1,4 @@
-const { SFI, LUMP_SUMS, SFI_PILOT } = require('../../../constants/schemes')
+const { SFI, LUMP_SUMS, SFI_PILOT, SFI23 } = require('../../../constants/schemes')
 const { createDefaultInvoiceNumber } = require('./create-default-invoice-number')
 const { createSitiAgriInvoiceNumber } = require('./create-siti-agri-invoice-number')
 
@@ -7,6 +7,7 @@ const createSplitInvoiceNumber = (invoiceNumber, splitId, schemeId) => {
     case SFI:
     case SFI_PILOT:
     case LUMP_SUMS:
+    case SFI23:
       return createSitiAgriInvoiceNumber(invoiceNumber, splitId)
     default:
       return createDefaultInvoiceNumber(invoiceNumber, splitId)
