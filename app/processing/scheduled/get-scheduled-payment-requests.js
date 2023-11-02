e52db575e99893c146c395b2cd9803ef30f01c39
John Watson <john.watson1@defra.gov.uk>
2023-11-02 12:09:29 +0000

    Split invoice line query

--- app/processing/scheduled/get-scheduled-payment-requests.js
+++ app/processing/scheduled/get-scheduled-payment-requests.js
@@ -2,6 +2,40 @@ const db = require('../../data')
 const moment = require('moment')
 
 const getScheduledPaymentRequests = async (started, transaction) => {
+  // const scheduledPaymentRequests = await db.schedule.findAll({
+  //   transaction,
+  //   lock: true,
+  //   skipLocked: true,
+  //   order: [['paymentRequest', 'paymentRequestNumber'], 'planned'],
+  //   include: [{
+  //     model: db.paymentRequest,
+  //     as: 'paymentRequest',
+  //     required: true,
+  //     include: [{
+  //       model: db.invoiceLine,
+  //       as: 'invoiceLines',
+  //       required: true,
+  //       where: {
+  //         invalid: { [db.Sequelize.Op.ne]: true }
+  //       }
+  //     }, {
+  //       model: db.scheme,
+  //       as: 'scheme',
+  //       required: true
+  //     }]
+  //   }],
+  //   where: {
+  //     '$paymentRequest.scheme.active$': true,
+  //     planned: { [db.Sequelize.Op.lte]: started },
+  //     completed: null,
+  //     [db.Sequelize.Op.or]: [{
+  //       started: null
+  //     }, {
+  //       started: { [db.Sequelize.Op.lte]: moment(started).subtract(5, 'minutes').toDate() }
+  //     }]
+  //   }
+  // })
+
   const scheduledPaymentRequests = await db.schedule.findAll({
     transaction,
     lock: true,
@@ -12,13 +46,6 @@ const getScheduledPaymentRequests = async (started, transaction) => {
       as: 'paymentRequest',
       required: true,
       include: [{
-        model: db.invoiceLine,
-        as: 'invoiceLines',
-        required: true,
-        where: {
-          invalid: { [db.Sequelize.Op.ne]: true }
-        }
-      }, {
         model: db.scheme,
         as: 'scheme',
         required: true
@@ -36,7 +63,21 @@ const getScheduledPaymentRequests = async (started, transaction) => {
     }
   })
 
-  return scheduledPaymentRequests.map(x => x.get({ plain: true }))
+  const plainScheduledPaymentRequests = scheduledPaymentRequests.map(x => x.get({ plain: true }))
+
+  // splitting for performance reasons
+  for (const plainScheduledPaymentRequest of plainScheduledPaymentRequests) {
+    plainScheduledPaymentRequest.paymentRequest.invoiceLines = await db.invoiceLine.findAll({
+      transaction,
+      where: {
+        paymentRequestId: plainScheduledPaymentRequest.paymentRequest.paymentRequestId,
+        invalid: { [db.Sequelize.Op.ne]: true }
+      },
+      raw: true
+    })
+  }
+
+  return plainScheduledPaymentRequests.filter(x => x.paymentRequest.invoiceLines.length)
 }
 
 module.exports = {
