4c77c2b862dfb9526face6eacb8d8530474efc7f
John Watson <john.watson1@defra.gov.uk>
2023-11-02 18:43:13 +0000

    Update pool

--- app/config/database.js
+++ app/config/database.js
@@ -23,6 +23,14 @@ const retry = {
   timeout: 360000
 }
 
+const pool = {
+  acquire: 120000,
+  evict: 1000,
+  idle: 10000,
+  max: 10,
+  min: 0
+}
+
 const config = {
   database: process.env.POSTGRES_DB || 'ffc_pay_processing',
   dialect: 'postgres',
@@ -35,6 +43,7 @@ const config = {
   port: process.env.POSTGRES_PORT || 5432,
   logging: process.env.POSTGRES_LOGGING || false,
   retry,
+  pool,
   schema: process.env.POSTGRES_SCHEMA_NAME || 'public',
   username: process.env.POSTGRES_USERNAME
 }
