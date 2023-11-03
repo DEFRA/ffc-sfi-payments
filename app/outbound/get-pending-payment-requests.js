908c33f016cde7c03a1be90395f6384c5f23878c
John Watson <john.watson1@defra.gov.uk>
2023-11-03 16:58:45 +0000

    Limit outbound WIP

--- app/outbound/get-pending-payment-requests.js
+++ app/outbound/get-pending-payment-requests.js
@@ -1,11 +1,13 @@
 const db = require('../data')
 const { removeNullProperties } = require('../remove-null-properties')
+const { processingConfig } = require('../config')
 
 const getPendingPaymentRequests = async (transaction) => {
   const paymentRequests = await db.completedPaymentRequest.findAll({
     transaction,
     lock: true,
     skipLocked: true,
+    limit: processingConfig.processingCap,
     include: [{
       model: db.completedInvoiceLine,
       as: 'invoiceLines',
