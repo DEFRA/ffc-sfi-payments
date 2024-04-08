8a12d1e6f7a5c0bf2a24bd0dc69cdca10c6d7741
Sam Plackett <samuel.plackett@eviden.com>
2024-04-08 16:25:38 +0100

    apply exchange rates to bps, fdmr payments

--- test/mocks/settlements/settlement.js
+++ test/mocks/settlements/settlement.js
@@ -1,9 +1,11 @@
 const { FRN } = require('../values/frn')
 const { INVOICE_NUMBER } = require('../values/invoice-number')
 const { TIMESTAMP } = require('../values/date')
+const { GBP } = require('../../../app/constants/currency')
 
 module.exports = {
   frn: FRN,
+  currency: GBP,
   invoiceNumber: INVOICE_NUMBER,
   settlementDate: TIMESTAMP,
   settled: true,
