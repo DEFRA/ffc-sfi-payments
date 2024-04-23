551b2e3c3492f4564f0c338402e72b6324a5b6a9
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2024-04-23 13:13:57 +0100

    exclude EXQ00 from previous domestic fund code checks (#183)

--- app/processing/dual-accounting/get-previous-domestic-fund.js
+++ app/processing/dual-accounting/get-previous-domestic-fund.js
@@ -1,9 +1,11 @@
 const domesticFundCodes = require('../../constants/domestic-fund-codes')
 
 const getPreviousDomesticFund = (paymentRequests) => {
+  const domesticFundCodesWithoutEXQ = { ...domesticFundCodes }
+  delete domesticFundCodesWithoutEXQ.EXQ00
   const previousPaymentRequestsWithInvoiceLines = paymentRequests.filter(x => x.invoiceLines?.length)
   return previousPaymentRequestsWithInvoiceLines[previousPaymentRequestsWithInvoiceLines.length - 1]?.invoiceLines
-    .filter(x => Object.values(domesticFundCodes).includes(x.fundCode))[0]?.fundCode
+    .filter(x => Object.values(domesticFundCodesWithoutEXQ).includes(x.fundCode))[0]?.fundCode
 }
 
 module.exports = {
--- test/unit/processing/dual-accounting/get-previous-domestic-fund.test.js
+++ test/unit/processing/dual-accounting/get-previous-domestic-fund.test.js
@@ -3,6 +3,8 @@ const domesticFundCodes = require('../../../../app/constants/domestic-fund-codes
 const { getPreviousDomesticFund } = require('../../../../app/processing/dual-accounting/get-previous-domestic-fund')
 
 let previousPaymentRequests
+const testDomesticFundCodes = domesticFundCodes
+delete testDomesticFundCodes.EXQ00
 
 describe('get previous domestic fund', () => {
   beforeEach(() => {
@@ -33,10 +35,16 @@ describe('get previous domestic fund', () => {
     expect(result).toBeUndefined()
   })
 
-  test.each(Object.values(domesticFundCodes))('should return domestic fund code if previous domestic fund used', (fundCode) => {
+  test.each(Object.values(testDomesticFundCodes))('should return domestic fund code if previous domestic fund used (with exception EXQ00)', (fundCode) => {
     previousPaymentRequests[0].invoiceLines[0].fundCode = fundCode
     const result = getPreviousDomesticFund(previousPaymentRequests)
     expect(result).toBe(fundCode)
+  })
+
+  test('should not return domestic fund code if EXQ00 used', () => {
+    previousPaymentRequests[0].invoiceLines[0].fundCode = domesticFundCodes.EXQ00
+    const result = getPreviousDomesticFund(previousPaymentRequests)
+    expect(result).toBeUndefined()
   })
 
   test('should return fund from last payment request', () => {
