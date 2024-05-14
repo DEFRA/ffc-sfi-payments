c9cf4c4eb021b744462ccbcc353348515bec5c36
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-05-14 17:24:53 +0100

    refactored code into two functions to split the complexity under the Sonar recommendation

--- app/processing/complete-payment-requests.js
+++ app/processing/complete-payment-requests.js
@@ -1,29 +1,40 @@
 const db = require('../data')
 
+const processPaymentRequest = async (paymentRequest, transaction) => {
+  const completedPaymentRequest = paymentRequest.dataValues ?? paymentRequest
+  const savedCompletedPaymentRequest = await db.completedPaymentRequest.create(completedPaymentRequest, { transaction })
+
+  for (const invoiceLine of paymentRequest.invoiceLines) {
+    await processInvoiceLine(invoiceLine, savedCompletedPaymentRequest, transaction)
+  }
+
+  if (!paymentRequest.invoiceLines.every(x => x.value === 0)) {
+    await db.outbox.create({ completedPaymentRequestId: savedCompletedPaymentRequest.completedPaymentRequestId }, { transaction })
+  }
+}
+
+const processInvoiceLine = async (invoiceLine, savedCompletedPaymentRequest, transaction) => {
+  const completedInvoiceLine = invoiceLine.dataValues ?? invoiceLine
+  completedInvoiceLine.completedPaymentRequestId = savedCompletedPaymentRequest.completedPaymentRequestId
+
+  if (completedInvoiceLine.value !== 0) {
+    await db.completedInvoiceLine.create(completedInvoiceLine, { transaction })
+  }
+}
+
 const completePaymentRequests = async (scheduleId, paymentRequests) => {
   const transaction = await db.sequelize.transaction()
   try {
     const schedule = await db.schedule.findByPk(scheduleId, { transaction })
-    // Check if completed already in case of duplicate processing
+
     if (schedule.completed === null) {
       await db.schedule.update({ completed: new Date() }, { where: { scheduleId }, transaction })
+
       for (const paymentRequest of paymentRequests) {
-        // Extract data values from Sequelize object if exists
-        const completedPaymentRequest = paymentRequest.dataValues ?? paymentRequest
-        const savedCompletedPaymentRequest = await db.completedPaymentRequest.create(completedPaymentRequest, { transaction })
-        for (const invoiceLine of paymentRequest.invoiceLines) {
-          // Extract data values from Sequelize object if exists
-          const completedInvoiceLine = invoiceLine.dataValues ?? invoiceLine
-          completedInvoiceLine.completedPaymentRequestId = savedCompletedPaymentRequest.completedPaymentRequestId
-          if (completedInvoiceLine.value !== 0) {
-            await db.completedInvoiceLine.create(completedInvoiceLine, { transaction })
-          }
-        }
-        if (!paymentRequest.invoiceLines.every(x => x.value === 0)) {
-          await db.outbox.create({ completedPaymentRequestId: savedCompletedPaymentRequest.completedPaymentRequestId }, { transaction })
-        }
+        await processPaymentRequest(paymentRequest, transaction)
       }
     }
+
     await transaction.commit()
   } catch (error) {
     await transaction.rollback()
