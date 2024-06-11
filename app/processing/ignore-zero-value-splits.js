9aaf3f35823dbb2f27c6b0ac42a4d028ef818309
Sam Plackett <samuel.plackett@eviden.com>
2024-06-11 10:16:12 +0100

    simplify prs to remove to one filter call

--- app/processing/ignore-zero-value-splits.js
+++ app/processing/ignore-zero-value-splits.js
@@ -5,22 +5,16 @@ const ignoreZeroValueSplits = (paymentRequests) => {
     return []
   }
 
-  const paymentRequestsToRemove = []
-
-  for (const paymentRequest of paymentRequests) {
-    for (const otherPaymentRequest of paymentRequests) {
-      if (
-        paymentRequest !== otherPaymentRequest &&
-        paymentRequest.ledger === AP &&
-        paymentRequest.ledger === otherPaymentRequest.ledger &&
-        paymentRequest.paymentRequestNumber === otherPaymentRequest.paymentRequestNumber &&
-        paymentRequest.originalInvoiceNumber === otherPaymentRequest.originalInvoiceNumber &&
-        paymentRequest.value === -otherPaymentRequest.value
-      ) {
-        paymentRequestsToRemove.push(paymentRequest.paymentRequestNumber)
-      }
-    }
-  }
+  const paymentRequestsToRemove = paymentRequests.filter(paymentRequest =>
+    paymentRequest.ledger === AP &&
+    paymentRequests.some(otherPaymentRequest =>
+      paymentRequest !== otherPaymentRequest &&
+      paymentRequest.ledger === otherPaymentRequest.ledger &&
+      paymentRequest.paymentRequestNumber === otherPaymentRequest.paymentRequestNumber &&
+      paymentRequest.originalInvoiceNumber === otherPaymentRequest.originalInvoiceNumber &&
+      paymentRequest.value === -otherPaymentRequest.value
+    )
+  ).map(paymentRequest => paymentRequest.paymentRequestNumber)
 
   return paymentRequests.filter(paymentRequest => !paymentRequestsToRemove.includes(paymentRequest.paymentRequestNumber))
 }
