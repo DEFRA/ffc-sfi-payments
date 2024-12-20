5ff1c547fd69aff43c58a655a1ef81094932b70b
Oliver Lewington <oliver.lewington@eviden.com>
2024-12-20 14:11:09 +0000

    Commiting Initial Changes to Service

--- app/messaging/process-return-message.js
+++ app/messaging/process-return-message.js
@@ -5,7 +5,17 @@ const { sendProcessingErrorEvent } = require('../event')
 const processReturnMessage = async (message, receiver) => {
   try {
     console.log('Return data received:', util.inspect(message.body, false, null, true))
+
+    const matches = text.match(/F\d{7}C\d{7}V\d{3}/g);
+
+    if (matches) {
+        console.log("Found matches:", matches);
+        console.log("FDMR invoice number detected, exiting.")
+        return;
+    }
+
     const settlementCompleted = await processSettlement(message.body)
+
     if (settlementCompleted) {
       await receiver.completeMessage(message)
       console.log('Settlement statuses updated from return file')
