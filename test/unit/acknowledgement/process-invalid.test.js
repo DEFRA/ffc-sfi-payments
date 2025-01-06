f1f448b87c388dfa43d951318b3717b71a6c9536
Oliver Lewington <oliver.lewington@eviden.com>
2025-01-06 12:50:07 +0000

    Incrememting version number

--- test/unit/acknowledgement/process-invalid.test.js
+++ test/unit/acknowledgement/process-invalid.test.js
@@ -32,68 +32,61 @@ jest.mock('../../../app/event')
 const { sendAcknowledgementErrorEvent: mockSendAcknowledgementErrorEvent } = require('../../../app/event')
 
 const acknowledgement = require('../../mocks/acknowledgement')
-const { FRN } = require('../../mocks/values/frn')
-
-const { DAX_REJECTION } = require('../../../app/constants/hold-categories-names')
-const { SFI } = require('../../../app/constants/schemes')
+const paymentRequest = require('../../mocks/payment-requests/payment-request')
 
 const { processInvalid } = require('../../../app/acknowledgement/process-invalid')
-
-const PAYMENT_REQUEST_ID = 1
-const HOLD_CATEGORY_ID = 1
-const SOURCE_SYSTEM = 'sourceSystem'
 
 describe('process invalid acknowledgements', () => {
   beforeEach(() => {
     jest.clearAllMocks()
 
-    mockGetHoldCategoryName.mockReturnValue(DAX_REJECTION)
-    mockGetHoldCategoryId.mockResolvedValue(HOLD_CATEGORY_ID)
+    mockGetHoldCategoryName.mockReturnValue('DAX_REJECTION')
+    mockGetHoldCategoryId.mockResolvedValue(1)
   })
 
   test('should create new database transaction', async () => {
-    await processInvalid(SFI, PAYMENT_REQUEST_ID, FRN, SOURCE_SYSTEM, acknowledgement)
+    await processInvalid(paymentRequest, acknowledgement)
     expect(mockTransaction).toHaveBeenCalledTimes(1)
   })
 
   test('should reset payment request by id in transaction scope', async () => {
-    await processInvalid(SFI, PAYMENT_REQUEST_ID, FRN, SOURCE_SYSTEM, acknowledgement)
-    expect(mockResetPaymentRequestById).toHaveBeenCalledWith(PAYMENT_REQUEST_ID, mockTransactionObject)
+    await processInvalid(paymentRequest, acknowledgement)
+    expect(mockResetPaymentRequestById).toHaveBeenCalledWith(paymentRequest.paymentRequestId, mockTransactionObject)
   })
 
   test('should get hold category name from acknowledgement message', async () => {
-    await processInvalid(SFI, PAYMENT_REQUEST_ID, FRN, SOURCE_SYSTEM, acknowledgement)
+    await processInvalid(paymentRequest, acknowledgement)
     expect(mockGetHoldCategoryName).toHaveBeenCalledWith(acknowledgement.message)
   })
 
   test('should get hold category id from scheme id and hold category name', async () => {
-    await processInvalid(SFI, PAYMENT_REQUEST_ID, FRN, SOURCE_SYSTEM, acknowledgement)
-    expect(mockGetHoldCategoryId).toHaveBeenCalledWith(SFI, DAX_REJECTION, mockTransactionObject)
+    await processInvalid(paymentRequest, acknowledgement)
+    expect(mockGetHoldCategoryId).toHaveBeenCalledWith(paymentRequest.schemeId, 'DAX_REJECTION', mockTransactionObject)
   })
 
   test('should hold and reschedule payment request', async () => {
-    await processInvalid(SFI, PAYMENT_REQUEST_ID, FRN, SOURCE_SYSTEM, acknowledgement)
-    expect(mockHoldAndReschedule).toHaveBeenCalledWith(PAYMENT_REQUEST_ID, HOLD_CATEGORY_ID, FRN, mockTransactionObject)
+    await processInvalid(paymentRequest, acknowledgement)
+    expect(mockHoldAndReschedule).toHaveBeenCalledWith(paymentRequest.paymentRequestId, 1, paymentRequest.frn, mockTransactionObject)
   })
 
   test('should send acknowledgement error event', async () => {
-    await processInvalid(SFI, PAYMENT_REQUEST_ID, FRN, SOURCE_SYSTEM, acknowledgement)
-    expect(mockSendAcknowledgementErrorEvent).toHaveBeenCalledWith(DAX_REJECTION, acknowledgement, FRN, SOURCE_SYSTEM)
+    await processInvalid(paymentRequest, acknowledgement)
+    expect(mockSendAcknowledgementErrorEvent).toHaveBeenCalledWith('DAX_REJECTION', acknowledgement, paymentRequest)
   })
 
   test('should commit transaction', async () => {
-    await processInvalid(SFI, PAYMENT_REQUEST_ID, FRN, SOURCE_SYSTEM, acknowledgement)
+    await processInvalid(paymentRequest, acknowledgement)
     expect(mockCommit).toHaveBeenCalledTimes(1)
   })
 
   test('should rollback transaction on error', async () => {
     mockHoldAndReschedule.mockRejectedValue(new Error('test error'))
-    await expect(processInvalid(SFI, PAYMENT_REQUEST_ID, FRN, SOURCE_SYSTEM, acknowledgement)).rejects.toThrow('test error')
+    await expect(processInvalid(paymentRequest, acknowledgement)).rejects.toThrow('test error')
     expect(mockRollback).toHaveBeenCalledTimes(1)
   })
 
   test('should throw error on error', async () => {
     mockHoldAndReschedule.mockRejectedValue(new Error('test error'))
-    await expect(processInvalid(SFI, PAYMENT_REQUEST_ID, FRN, SOURCE_SYSTEM, acknowledgement)).rejects.toThrow('test error')
+    await expect(processInvalid(paymentRequest, acknowledgement)).rejects.toThrow('test error')
   })
 })
--- test/unit/event/send-acknowledgement-error-event.test.js
+++ test/unit/event/send-acknowledgement-error-event.test.js
@@ -17,8 +17,7 @@ const { BANK_ACCOUNT_ANOMALY } = require('../../../app/constants/hold-categories
 const { sendAcknowledgementErrorEvent } = require('../../../app/event/send-acknowledgement-error-event')
 
 const acknowledgement = require('../../mocks/acknowledgement')
-const frn = require('../../mocks/values/frn')
-const SOURCE_SYSTEM = 'sourceSystem'
+const paymentRequest = require('../../mocks/payment-requests/payment-request')
 
 describe('send acknowledgement error event', () => {
   beforeEach(() => {
@@ -26,24 +25,24 @@ describe('send acknowledgement error event', () => {
   })
 
   test('should send invalid bank details error event if hold category is bank account anomaly', async () => {
-    await sendAcknowledgementErrorEvent(BANK_ACCOUNT_ANOMALY, acknowledgement, frn, SOURCE_SYSTEM)
-    expect(mockSendAckInvalidBankDetailsErrorEvent).toHaveBeenCalledWith(frn, SOURCE_SYSTEM)
+    await sendAcknowledgementErrorEvent(BANK_ACCOUNT_ANOMALY, acknowledgement, paymentRequest)
+    expect(mockSendAckInvalidBankDetailsErrorEvent).toHaveBeenCalledWith(paymentRequest)
   })
 
   test('should send processing acknowledgement error event if hold category is not bank account anomaly', async () => {
     const holdCategoryName = 'OTHER_CATEGORY'
-    await sendAcknowledgementErrorEvent(holdCategoryName, acknowledgement, frn, SOURCE_SYSTEM)
+    await sendAcknowledgementErrorEvent(holdCategoryName, acknowledgement, paymentRequest)
     expect(mockSendProcessingAckErrorEvent).toHaveBeenCalledWith(acknowledgement)
   })
 
   test('should not send processing acknowledgement error event if hold category is bank account anomaly', async () => {
-    await sendAcknowledgementErrorEvent(BANK_ACCOUNT_ANOMALY, acknowledgement, frn, SOURCE_SYSTEM)
+    await sendAcknowledgementErrorEvent(BANK_ACCOUNT_ANOMALY, acknowledgement, paymentRequest)
     expect(mockSendProcessingAckErrorEvent).not.toHaveBeenCalled()
   })
 
   test('should not send invalid bank details error event if hold category is not bank account anomaly', async () => {
     const holdCategoryName = 'OTHER_CATEGORY'
-    await sendAcknowledgementErrorEvent(holdCategoryName, acknowledgement, frn, SOURCE_SYSTEM)
+    await sendAcknowledgementErrorEvent(holdCategoryName, acknowledgement, paymentRequest)
     expect(mockSendAckInvalidBankDetailsErrorEvent).not.toHaveBeenCalled()
   })
 })
