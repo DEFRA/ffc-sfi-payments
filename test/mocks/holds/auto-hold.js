a835448f68916cdf45f80f58ac74e7def51cbdeb
Sam Plackett <samuel.plackett@eviden.com>
2025-02-25 14:30:51 +0000

    introduce contractNumber (claim id) to auto holds

--- test/mocks/holds/auto-hold.js
+++ test/mocks/holds/auto-hold.js
@@ -2,6 +2,7 @@ const { FRN } = require('../values/frn')
 const { TIMESTAMP } = require('../values/date')
 const { MARKETING_YEAR } = require('../values/marketing-year')
 const { AGREEMENT_NUMBER } = require('../values/agreement-number')
+const { CONTRACT_NUMBER } = require('../values/contract-number')
 
 module.exports = {
   autoHoldId: 1,
@@ -9,5 +10,6 @@ module.exports = {
   frn: FRN,
   marketingYear: MARKETING_YEAR,
   agreementNumber: AGREEMENT_NUMBER,
+  contractNumber: CONTRACT_NUMBER,
   added: TIMESTAMP
 }
